c887bd61e23841257237b3b675fb8ffd157a1a36
Paul Stovell <paul@paulstovell.com>
2012-08-20 17:45:15 +0100

    Added a test for cmd line arg parsing

---
+++ source/OctopusTools.Tests/Commands/OptionsFixture.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using OctopusTools.Commands;
+
+namespace OctopusTools.Tests.Commands
+{
+    [TestFixture]
+    public class OptionsFixture
+    {
+        [Test]
+        [TestCase("--apiKey=abc123")]
+        [TestCase("--apikey=abc123")]
+        [TestCase("-apikey=abc123")]
+        [TestCase("--apikey=abc123")]
+        [TestCase("/apikey=abc123")]
+        public void ShouldBeCaseInsensitive(string parameter)
+        {
+            var apiKey = string.Empty;
+            var optionSet = new OptionSet()
+            {
+                {"apiKey=", "API key", v => apiKey = v}
+            };
+
+            optionSet.Parse(new[] {parameter});
+
+            Assert.That(apiKey, Is.EqualTo("abc123"));
+        }
+    }
+}
