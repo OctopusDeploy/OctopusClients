90fdfcae37acef95b0198067757213964a0f6058
Tom Parker <tom.parker@spotlight.com>
2013-04-25 11:46:25 +0100

    Add test for extra options

---
+++ source/OctopusTools.Tests/Commands/CommandProcessorFixture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NSubstitute;
+using NUnit.Framework;
+using OctopusTools.Infrastructure;
+using log4net;
+
+namespace OctopusTools.Tests.Commands
+{
+    [TestFixture]
+    public class CommandProcessorFixture
+    {
+        [Test]
+        public void ShouldFailWithExtraOptions()
+        {
+            var commandLocator = Substitute.For<ICommandLocator>();
+            commandLocator.Find("speak").Returns(new SpeakCommand());
+            var log = Substitute.For<ILog>();
+            var cp = new CommandProcessor(commandLocator, log);
+            var ae = Assert.Throws<ApplicationException>(() => cp.Process(new[] {"speak", "--foo"}));
+            Assert.NotNull(ae.InnerException);
+            Assert.AreEqual("Extra non-recognized parameters for command: --foo", ae.InnerException.Message);
+        }
+    }
+}
