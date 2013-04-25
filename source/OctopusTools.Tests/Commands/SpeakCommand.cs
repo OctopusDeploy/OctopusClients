8a0bb63f1cea5ea8a63c8a4e0dec8f5213de1f9e
Tom Parker <tom.parker@spotlight.com>
2013-04-25 11:45:49 +0100

    Pull SpeakCommand into a separate file

---
+++ source/OctopusTools.Tests/Commands/SpeakCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+using OctopusTools.Commands;
+using OctopusTools.Infrastructure;
+
+namespace OctopusTools.Tests.Commands
+{
+    public class SpeakCommand : ICommand {
+        public OptionSet Options {
+            get {
+                var options = new OptionSet();
+                options.Add("message=", m => { });
+                return options;
+            }
+        }
+
+        public virtual void Execute() {
+            Assert.Fail("This should not be called");
+        }
+    }
+}
