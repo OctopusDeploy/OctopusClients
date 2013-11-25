74f4ddfb8dde01a834ad1735e1aec7f12b179ed1
Jeff French <jeff.french@moonswitch.com>
2013-11-25 04:02:34 +0000

    Update list-latestdeployments for 2.0.

--- source/OctopusTools/Commands/ListLatestDeploymentsCommand.cs
+++ source/OctopusTools/Commands/ListLatestDeploymentsCommand.cs
@@ -1,112 +1,119 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using OctopusTools.Infrastructure;
-//using OctopusTools.Model;
-//using log4net;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using NuGet;
+using Octopus.Client;
+using Octopus.Client.Model;
+using log4net;
 
-//namespace OctopusTools.Commands
-//{
-//    [Command("list-latestdeployments", Description = "List the releases last-deployed in each environment")]
-//    public class ListLatestDeploymentsCommand : ApiCommand
-//    {
-//        readonly HashSet<string> environments = new HashSet<string>(StringComparer.OrdinalIgnoreCase); 
-//        readonly HashSet<string> projects = new HashSet<string>(StringComparer.OrdinalIgnoreCase); 
+namespace OctopusTools.Commands
+{
+    [Command("list-latestdeployments", Description = "List the releases last-deployed in each environment")]
+    public class ListLatestDeploymentsCommand : ApiCommand
+    {
+        readonly HashSet<string> environments = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+        readonly HashSet<string> projects = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
 
-//        public ListLatestDeploymentsCommand(IOctopusRepositoryFactory repositoryFactory, ILog log) : base(repositoryFactory, log)
-//        {
-//        }
+        public ListLatestDeploymentsCommand(IOctopusRepositoryFactory repositoryFactory, ILog log)
+            : base(repositoryFactory, log)
+        {
+        }
 
-//        protected override void SetOptions(OptionSet options)
-//        {
-//            options.Add("project=", "Name of a project to filter by. Can be specified many times.", v => projects.Add(v));
-//            options.Add("environment=", "Name of an environment to filter by. Can be specified many times.", v => environments.Add(v));
-//        }
+        protected override void SetOptions(OptionSet options)
+        {
+            options.Add("project=", "Name of a project to filter by. Can be specified many times.", v => projects.Add(v));
+            options.Add("environment=", "Name of an environment to filter by. Can be specified many times.", v => environments.Add(v));
+        }
 
-//        protected override void Execute()
-//        {
-//            var projectsFilter = new string[0];
-//            if (projects.Count > 0)
-//            {
-//                Log.Debug("Loading projects...");
-//                projectsFilter = Repository.Projects.FindByNames(projects).Select(p => p.Id).ToArray();
-//            }
-            
-//            var environmentsFilter = new string[0];
-//            if (environments.Count > 0)
-//            {
-//                Log.Debug("Loading environments...");
-//                environmentsFilter = Repository.Environments.FindByNames(environments).Select(p => p.Id).ToArray();
-//            }
+        protected override void Execute()
+        {
+            var projectsFilter = new string[0];
+            if (projects.Count > 0)
+            {
+                Log.Debug("Loading projects...");
+                projectsFilter = Repository.Projects.FindByNames(projects.ToArray()).Select(p => p.Id).ToArray();
+            }
 
-//            //var projects = Repository.Projects
+            var environmentsById = new Dictionary<string, string>();
+            if (environments.Count > 0)
+            {
+                Log.Debug("Loading environments...");
+                environmentsById.AddRange(Repository.Environments.FindByNames(environments.ToArray()).Select(p => new KeyValuePair<string, string>(p.Id, p.Name)));
+            }
+            else
+            {
+                environmentsById.AddRange(Repository.Environments.FindAll().Select(p => new KeyValuePair<string, string>(p.Id, p.Name)));
+            }
 
-//            Log.Debug("Finding most recent deployments...");
+            var deployments = Repository.Deployments.FindAll(projectsFilter, environments.Count > 0 ? environmentsById.Keys.ToArray() : new string[] { });
 
-//            var deployments = Repository.Deployments.FindAll(projectsFilter, environmentsFilter);
+            foreach (var deployment in deployments.Items)
+            {
+                LogDeploymentInfo(deployment, environmentsById);
+            }
+        }
 
-//            foreach (var deployment in deployments)
-//            {
-//                var nameOfDeploymentEnvironment = environmentsById[deployment.EnvironmentId].Name;
-//                if (EnvironmentName != null && !string.Equals(EnvironmentName, nameOfDeploymentEnvironment, StringComparison.InvariantCultureIgnoreCase))
-//                {
-//                    continue;
-//                }
+        public void LogDeploymentInfo(DeploymentResource deployment, Dictionary<string, string> environmentsById)
+        {
+            var nameOfDeploymentEnvironment = environmentsById[deployment.EnvironmentId];
+            var taskId = deployment.Link("Task");
+            var task = Repository.Tasks.Get(taskId);
+            var release = Repository.Releases.Get(deployment.Link("Release"));
 
-//                var task = Session.Get<Task>(deployment.Link("Task"));
-//                var release = Session.Get<Release>(deployment.Link("Release"));
+            var propertiesToLog = new List<string>();
+            propertiesToLog.AddRange(FormatTaskPropertiesAsStrings(task));
+            propertiesToLog.AddRange(FormatReleasePropertiesAsStrings(release));
+            Log.InfoFormat(" - Environment: {0}", nameOfDeploymentEnvironment);
+            foreach (var property in propertiesToLog)
+            {
+                if(property == "State: Failed")
+                    Log.ErrorFormat("   {0}", property);
+                else
+                    Log.InfoFormat("   {0}", property);
+            }
+            Log.InfoFormat("");
+        }
 
-//                var propertiesToLog = new List<string>();
-//                propertiesToLog.AddRange(FormatTaskPropertiesAsStrings(task));
-//                propertiesToLog.AddRange(FormatReleasePropertiesAsStrings(release));
 
-//                Log.InfoFormat(" - Environment: {0}", nameOfDeploymentEnvironment);
-//                foreach (var property in propertiesToLog)
-//                {
-//                    Log.InfoFormat("   {0}", property);
-//                }
-//                Log.InfoFormat("");
-//            }
-//        }
+        private static IEnumerable<string> FormatTaskPropertiesAsStrings(TaskResource task)
+        {
+            return new List<string>{
+                    "Date: " + task.QueueTime,
+                    "Duration: " + task.Duration,
+                    "State: "+task.State                  
+                };
+        }
 
-//        private static IEnumerable<string> FormatTaskPropertiesAsStrings(Task task)
-//        {
-//            return new List<string>{
-//                "Date: " + task.QueueTime,
-//                "Duration: " + task.Duration,
-//                "State: "+task.State                  
-//            };
-//        }
+        private static IEnumerable<string> FormatReleasePropertiesAsStrings(ReleaseResource release)
+        {
+            return new List<string>{
+                    "Version: " + release.Version,
+                    "Assembled: "+release.Assembled,
+                    "Package Versions: " + GetPackageVersionsAsString(release.SelectedPackages),
+                    "Release Notes: " + ((release.ReleaseNotes != null) ? release.ReleaseNotes.Replace(System.Environment.NewLine,@"\n"): "")
+                };
+        }
 
-//        private static IEnumerable<string> FormatReleasePropertiesAsStrings(Release release)
-//        {
-//            return new List<string>{
-//                "Version: " + release.Version,
-//                "Assembled: "+release.Assembled,
-//                "Package Versions: " + GetPackageVersionsAsString(release.PackageVersions),
-//                "Release Notes: " + ((release.ReleaseNotes != null) ? release.ReleaseNotes.Replace(System.Environment.NewLine,@"\n"): "")
-//            };
-//        }
+        private static string GetPackageVersionsAsString(IEnumerable<SelectedPackage> packages)
+        {
+            var packageVersionsAsString = "";
 
-//        private static string GetPackageVersionsAsString(IEnumerable<PackageVersion> packageVersions)
-//        {
-//            var packageVersionsAsString = "";
-
-//            foreach (var pv in packageVersions)
-//            {
-//                var packageVersionAsString = pv.Id + " " + pv.Version;
-
-//                if (packageVersionsAsString.Contains(packageVersionAsString))
-//                {
-//                    continue;
-//                }
-//                if (!String.IsNullOrEmpty(packageVersionsAsString))
-//                {
-//                    packageVersionsAsString += "; ";
-//                }
-//                packageVersionsAsString += packageVersionAsString;
-//            }
-//            return packageVersionsAsString;
-//        }
-//    }
-//}
+            foreach (var package in packages)
+            {
+                var packageVersionAsString = package.StepName  + " " + package.Version;
+              
+                if (packageVersionsAsString.Contains(packageVersionAsString))
+                {
+                    continue;
+                }
+                if (!String.IsNullOrEmpty(packageVersionsAsString))
+                {
+                    packageVersionsAsString += "; ";
+                }
+                packageVersionsAsString += packageVersionAsString;
+            }
+            return packageVersionsAsString;
+        }
+    }
+}
