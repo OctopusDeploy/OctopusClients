a95a8776f750e1fd0c577c6140ea70d19029ed68
nblumhardt <nblumhardt@nblumhardt.com>
2014-03-21 08:49:27 +1000

    Tidy up references.

--- source/OctopusTools/Commands/CreateReleaseCommand.cs
+++ source/OctopusTools/Commands/CreateReleaseCommand.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using NuGet;
 using Octopus.Client.Model;
 using OctopusTools.Diagnostics;
 using OctopusTools.Infrastructure;
