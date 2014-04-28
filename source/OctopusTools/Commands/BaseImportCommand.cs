de2984dbefeb9b0a23f24ffa4da49d3fbcf57404
Henrik Andersson <henrik.a.andersson@outlook.com>
2014-04-28 16:59:24 +1000

    Implement feeds import command

---
+++ source/OctopusTools/Commands/BaseImportCommand.cs
@@ -0,0 +1,44 @@
+﻿using log4net;
+using Octopus.Platform.Util;
+using OctopusTools.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace OctopusTools.Commands
+{
+    public abstract class BaseImportCommand : ApiCommand
+    {
+        readonly IOctopusFileSystem fileSystem;
+
+        public BaseImportCommand(IOctopusFileSystem fileSystem, IOctopusRepositoryFactory repositoryFactory, ILog log)
+            : base(repositoryFactory, log)
+        {
+            this.fileSystem = fileSystem;
+        }
+
+        public string GetSerializedObjectFromFile(string filePath)
+        {
+            Log.Debug("Loading export file");
+            if (!fileSystem.FileExists(filePath))
+                throw new CommandException("Unable to find the specified export file");
+
+            var export = string.Empty;
+            try
+            {
+                var file = fileSystem.OpenFile(filePath, FileAccess.Read, FileShare.Read);
+                var bytes = new byte[file.Length];
+                var bytesRead = file.Read(bytes, 0, (int)file.Length);
+                export = Encoding.UTF8.GetString(bytes);
+            }
+            catch (Exception ex)
+            {
+                throw new CommandException("Unable to read the specified export file. Error: " + ex.Message);
+            }
+            Log.Debug("Export file loaded");
+            return export;
+        }
+    }
+}
---
+++ source/OctopusTools/Commands/ImportFeedsCommand.cs
@@ -0,0 +1,66 @@
+﻿using log4net;
+using Newtonsoft.Json;
+using Octopus.Client.Model;
+using Octopus.Platform.Util;
+using OctopusTools.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OctopusTools.Commands
+{
+    [Command("import-feeds", Description="Imports external feeds")]
+    public class ImportFeedsCommand : BaseImportCommand
+    {
+        public ImportFeedsCommand(IOctopusFileSystem fileSystem, IOctopusRepositoryFactory repositoryFactory, ILog log)
+            : base(fileSystem, repositoryFactory, log)
+        {
+        }
+
+        public string FilePath { get; set; }
+
+        protected override void SetOptions(OptionSet options)
+        {
+            options.Add("filePath=", "Full path and name of the export file to be imported", v => FilePath = v);
+        }
+
+        protected override void Execute()
+        {
+            if (string.IsNullOrWhiteSpace(FilePath)) throw new CommandException("Please specify the full path and name of the export file to be imported using the parameter: --filePath=XYZ");
+
+            var feeds = JsonConvert.DeserializeObject<List<FeedResource>>(GetSerializedObjectFromFile(FilePath));
+            if(feeds.Count == 0)
+            {
+                Log.Debug("Found no feeds for import.");
+                return;
+            }
+
+            try
+            {
+                foreach (var feed in feeds)
+                {
+                    var existingFeed = Repository.Feeds.FindByName(feed.Name);
+                    if (existingFeed != null)
+                    {
+                        existingFeed.FeedUri = feed.FeedUri;
+                        existingFeed.Username = feed.Username;
+
+                        Repository.Feeds.Modify(existingFeed);
+                        Log.DebugFormat("Updated existing feed. ID: {0}", existingFeed.Id);
+                    } 
+                    else
+                    {
+                        var newFeed = Repository.Feeds.Create(feed);
+                        Log.DebugFormat("Created new feed. ID: {0}", newFeed.Id);
+                    }
+                }
+                Log.DebugFormat("Successfully imported {0} {1}", feeds.Count, feeds.Count == 1 ? "feed" : "feeds");
+            }
+            catch(Exception ex)
+            {
+                throw new CommandException(string.Format("Failed to import feeds. Error: {0}", ex.Message));
+            }
+        }
+    }
+}
