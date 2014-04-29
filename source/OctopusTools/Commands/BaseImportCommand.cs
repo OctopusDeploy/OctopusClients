195b0c263550b6610d86364823b27f8c6b0cf9bd
Henrik Andersson <henrik.a.andersson@outlook.com>
2014-04-30 08:16:10 +1000

    Some R# refactoring

--- source/OctopusTools/Commands/BaseImportCommand.cs
+++ source/OctopusTools/Commands/BaseImportCommand.cs
@@ -2,9 +2,7 @@
 using Octopus.Platform.Util;
 using OctopusTools.Infrastructure;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 
 namespace OctopusTools.Commands
@@ -13,7 +11,7 @@ namespace OctopusTools.Commands
     {
         readonly IOctopusFileSystem fileSystem;
 
-        public BaseImportCommand(IOctopusFileSystem fileSystem, IOctopusRepositoryFactory repositoryFactory, ILog log)
+        protected BaseImportCommand(IOctopusFileSystem fileSystem, IOctopusRepositoryFactory repositoryFactory, ILog log)
             : base(repositoryFactory, log)
         {
             this.fileSystem = fileSystem;
@@ -25,18 +23,19 @@ namespace OctopusTools.Commands
             if (!fileSystem.FileExists(filePath))
                 throw new CommandException("Unable to find the specified export file");
 
-            var export = string.Empty;
+            string export;
             try
             {
-                var file = fileSystem.OpenFile(filePath, FileAccess.Read, FileShare.Read);
+                var file = fileSystem.OpenFile(filePath, FileAccess.Read);
                 var bytes = new byte[file.Length];
-                var bytesRead = file.Read(bytes, 0, (int)file.Length);
+                file.Read(bytes, 0, (int)file.Length);
                 export = Encoding.UTF8.GetString(bytes);
             }
             catch (Exception ex)
             {
                 throw new CommandException("Unable to read the specified export file. Error: " + ex.Message);
             }
+
             Log.Debug("Export file loaded");
             return export;
         }
