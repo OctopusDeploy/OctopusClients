539e26e95b949b37bc678b8f86adecb744b097b4
Shane Gill <tothegills@gmail.com>
2015-10-19 11:51:49 +1000

    Add lifecycle to create-project
    Fixes OctopusDeploy/Issues#1503

--- source/OctopusTools/Commands/CreateProjectCommand.cs
+++ source/OctopusTools/Commands/CreateProjectCommand.cs
@@ -14,17 +14,20 @@ namespace OctopusTools.Commands
             var options = Options.For("Project creation");
             options.Add("name=", "The name of the project", v => ProjectName = v);
             options.Add("projectGroup=", "The name of the project group to add this project to. If the group doesn't exist, it will be created.", v => ProjectGroupName = v);
+            options.Add("lifecycle=", "The name of the lifecycle that the project will use.", v=> LifecycleName = v);
             options.Add("ignoreIfExists", "If the project already exists, an error will be returned. Set this flag to ignore the error.", v => IgnoreIfExists = true);
         }
 
         public string ProjectName { get; set; }
         public string ProjectGroupName { get; set; }
         public bool IgnoreIfExists { get; set; }
+        public string LifecycleName { get; set; }
 
         protected override void Execute()
         {
             if (string.IsNullOrWhiteSpace(ProjectGroupName)) throw new CommandException("Please specify a project group name using the parameter: --projectGroup=XYZ");
             if (string.IsNullOrWhiteSpace(ProjectName)) throw new CommandException("Please specify a project name using the parameter: --name=XYZ");
+            if (string.IsNullOrWhiteSpace(LifecycleName)) throw new CommandException("Please specify a lifecycle name using the parameter: --lifecycle=XYZ");
 
             Log.Info("Finding project group: " + ProjectGroupName);
             var group = Repository.ProjectGroups.FindByName(ProjectGroupName);
@@ -33,6 +36,11 @@ namespace OctopusTools.Commands
                 Log.Info("Project group does not exist, it will be created");
                 group = Repository.ProjectGroups.Create(new ProjectGroupResource {Name = ProjectGroupName});
             }
+
+            Log.Info("Finding lifecycle: " + LifecycleName);
+            var lifecycle = Repository.Lifecycles.FindOne(l => l.Name.Equals(LifecycleName, StringComparison.Ordinal));
+            if (lifecycle == null)
+                throw new CommandException("The lifecycle " + LifecycleName + " does not exist.");
 
             var project = Repository.Projects.FindByName(ProjectName);
             if (project != null)
@@ -45,9 +53,9 @@ namespace OctopusTools.Commands
 
                 throw new CommandException("The project " + project.Name + " (ID " + project.Id + ") already exists in this project group.");
             }
-
+            
             Log.Info("Creating project: " + ProjectName);
-            project = Repository.Projects.Create(new ProjectResource {Name = ProjectName, ProjectGroupId = group.Id, IsDisabled = false});
+            project = Repository.Projects.Create(new ProjectResource {Name = ProjectName, ProjectGroupId = group.Id, IsDisabled = false, LifecycleId = lifecycle.Id});
 
             Log.Info("Project created. ID: " + project.Id);
         }
