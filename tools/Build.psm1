c8c327abcd771e59393fb94030c7afcb6bdb6f52
michaelnoonan <michaelnoonan@gmail.com>
2015-11-13 16:53:34 +1000

    Use ILRepack - ILMerge hangs with latest Octostache/Sprache.

--- tools/Build.psm1
+++ tools/Build.psm1
@@ -1,12 +1,12 @@
 $msbuild = "MSBuild.exe"
 
-function Create-OctopusToolsRelease ([string]$BuildNumber=0.0.0)
+function Create-OctopusToolsRelease ([string]$BuildNumber="0.0.0")
 {
     <#
     .Synopsis
      Builds a complete release (compile, test, ILMerge, NuGet package, version stamp)
     #>
-	& $msbuild "Tools\Build.proj" /p:build_number=$BuildNumber /t:BuildAndPublish /verbosity:detailed
+	& $msbuild "Tools\Build.proj" /p:build_number=$BuildNumber /t:BuildAndPublish /v:d
 }
 
 function Create-OctopusToolsPackage
@@ -15,7 +15,7 @@ function Create-OctopusToolsPackage
     .Synopsis
         Compiles, tests, runs ILMerge
     #>
-	& $msbuild "Tools\Build.proj" /t:BuildAndPackage /v:q
+	& $msbuild "Tools\Build.proj" /t:BuildAndPackage /v:d
 }
 
 Export-ModuleMember -function * -alias *
