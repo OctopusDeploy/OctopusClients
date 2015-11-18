a0794b43b114d7032b75b1b15bfdfd00115791ed
michaelnoonan <michaelnoonan@gmail.com>
2015-11-19 09:03:15 +1000

    Removed old files left on disk, but not in Project.

--- source/OctopusTools/Model/ApiConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OctopusTools.Model
-{
-    public class ApiConstants
-    {
-        public const string SupportedApiSchemaVersionMin = "1.0.0";
-        public const string SupportedApiSchemaVersionMax = "2.0.99";
-        public const string ApiHttpHeaderName = "X-Octopus-ApiKey"; 
-    }
-}
