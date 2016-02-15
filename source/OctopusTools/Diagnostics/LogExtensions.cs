696753e48c22c6483e2b798e79c247a9a555a1df
Damian Brady <dev@damianbrady.com.au>
2016-02-15 15:51:25 +1000

    Web link rather than API

--- source/OctopusTools/Diagnostics/LogExtensions.cs
+++ source/OctopusTools/Diagnostics/LogExtensions.cs
@@ -111,7 +111,7 @@ namespace OctopusTools.Diagnostics
             if (buildEnvironment == BuildEnvironment.TeamFoundationBuild || buildEnvironment == BuildEnvironment.NoneOrUnknown)
             {
 
-                var selflink = release.Links["Self"].AsString();
+                var selflink = release.Links["Web"].AsString();
                 selflink = new Uri(new Uri(serverBaseUrl), selflink.Substring(0, selflink.IndexOf("{", StringComparison.Ordinal))).ToString();
                 var markdown = string.Format("[Release {0} created for '{1}']({2})", release.Version, project.Name, selflink);
                 var fileguid = Guid.NewGuid() + ".md";
