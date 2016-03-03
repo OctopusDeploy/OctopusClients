c8365ecfe88359851a846851e847c1f8b283bf2c
Paul Stovell <paul.stovell@octopus.com>
2016-03-03 14:25:22 +1000

    OctopusTools => Octopus.Cli

---
+++ source/Octopus.Cli.Tests/Commands/SpeakCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using OctopusTools.Commands;
+using OctopusTools.Infrastructure;
+
+namespace OctopusTools.Tests.Commands
+{
+    public class SpeakCommand : ICommand
+    {
+        public OptionSet Options
+        {
+            get
+            {
+                var options = new OptionSet();
+                options.Add("message=", m => { });
+                return options;
+            }
+        }
+
+        public void GetHelp(TextWriter writer)
+        {
+            writer.WriteLine("message=");
+            Options.WriteOptionDescriptions(writer);
+        }
+
+        public void Execute(string[] commandLineArguments)
+        {
+            Assert.Fail("This should not be called");
+        }
+    }
+}
