578103b3596a6f5c55027638ca3eda21d1ee86e6
robert <robert.erez@gmail.com>
2016-07-07 09:45:32 +1000

    add ListTenantsCommand

---
+++ source/Octopus.Cli/Commands/ListTenantsCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using log4net;
+using Octopus.Cli.Infrastructure;
+using Octopus.Cli.Util;
+
+namespace Octopus.Cli.Commands
+{
+    [Command("list-tenants", Description = "List tenants")]
+    public class ListTenantsCommand : ApiCommand
+    {
+
+        public ListTenantsCommand(IOctopusRepositoryFactory repositoryFactory, ILog log, IOctopusFileSystem fileSystem)
+            : base(repositoryFactory, log, fileSystem)
+        {
+        }
+
+        protected override void Execute()
+        {
+            var tenants = Repository.Tenants.FindAll();
+            Log.Info("Tenants: " + tenants.Count);
+
+            foreach (var tenant in tenants.OrderBy(m => m.Name))
+            {
+                Log.InfoFormat(" - {0} (ID: {1})", tenant.Name, tenant.Id);
+            }
+        }
+    }
+}
