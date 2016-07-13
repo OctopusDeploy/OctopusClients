70f3c7ca8e8a7a01e78cfa096740358fc30b5893
robert <robert.erez@gmail.com>
2016-07-13 11:37:24 +1000

    Added test to ensure if `Octopus.Client` is a pre-release, then `Octo.exe` must be pre-release (and vice versa)

---
+++ source/Octopus.Cli.Tests/VersioningFixture.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+using Octopus.Cli.Tests.Helpers;
+using Octopus.Client;
+
+namespace Octopus.Cli.Tests
+{
+    [TestFixture]
+    public class VersioningFixture
+    {
+        [Test]
+        public void TheTestAssemblyIsVersioned_WithGitVersion()
+        {
+            Assert.That(new SemanticVersionInfo(GetType().Assembly).Major, Is.GreaterThan(1));
+        }
+
+        [Test]
+        public void AllOctopusAssemblies_ShouldHaveTheSameSemanticVersion()
+        {
+            var octocliVersion = (new SemanticVersionInfo(typeof(Octopus.Cli.Program).Assembly));
+            var octopusClientVersion = new SemanticVersionInfo(typeof(IOctopusRepository).Assembly);
+
+            var isClientPreRelease = !string.IsNullOrEmpty(octopusClientVersion.PreReleaseTag);
+            var isThisPreRelease = !string.IsNullOrEmpty(octocliVersion.PreReleaseTag);
+
+            Console.WriteLine($"Octopus.Client: {octopusClientVersion.SemVer}");
+            Console.WriteLine($"Octopus.Cli (Octo.exe): {octocliVersion.SemVer}");
+
+            Assert.That(!(isClientPreRelease ^ isThisPreRelease), "Octo.exe must be a pre-release if Octopus.Client is a pre-release, and non pre-release if Octopus.Client is not a pre-release");
+        }
+    }
+}
