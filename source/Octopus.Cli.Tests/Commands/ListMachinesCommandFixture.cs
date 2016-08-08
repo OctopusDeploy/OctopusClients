00dd919e97a062ed96f4320da2f8a23fa800173b
robert <robert.erez@gmail.com>
2016-08-08 16:09:37 +1000

    Add some extra unit tests to cover listing machines with the new health status

--- source/Octopus.Cli.Tests/Commands/ListMachinesCommandFixture.cs
+++ source/Octopus.Cli.Tests/Commands/ListMachinesCommandFixture.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using NSubstitute;
 using NUnit.Framework;
 using Octopus.Cli.Commands;
+using Octopus.Cli.Infrastructure;
 using Octopus.Client.Model;
+using Octopus.Client.Model.Endpoints;
+
 #pragma warning disable 618
 
 namespace Octopus.Cli.Tests.Commands
@@ -165,6 +168,156 @@ namespace Octopus.Cli.Tests.Commands
             Log.DidNotReceive().InfoFormat(MachineLogFormat, "PC01234", MachineModelStatus.Online.ToString(), "Machines-001", "Development");
             Log.DidNotReceive().InfoFormat(MachineLogFormat, "PC01466", MachineModelStatus.Online.ToString(), "Machines-002", "Development");
             Log.Received().InfoFormat(MachineLogFormat, "PC01996", MachineModelStatus.Offline.ToString(), "Machines-003", "Development");
+        }
+
+        [Test]
+        public void ShouldGetListOfMachinesWithMachineHealthStatusArgs()
+        {
+            CommandLineArgs.Add("--health-status=HasWarnings");
+            Repository.Client.RootDocument.Version = "3.5.0";
+            Repository.Environments.FindAll().Returns(new List<EnvironmentResource>
+            {
+                new EnvironmentResource {Name = "Development", Id = "Environments-001"}
+            });
+
+            Repository.Machines.FindAll().Returns(new List<MachineResource>
+            {
+                new MachineResource {
+                    Name = "PC0123",
+                    Id = "Machines-001",
+                    Status = MachineModelStatus.Online,
+                    HealthStatus = MachineModelHealthStatus.Unavailable,
+                    EnvironmentIds = new ReferenceCollection("Environments-001")
+                },
+                new MachineResource {
+                    Name = "PC01466",
+                    Id = "Machines-002",
+                    Status = MachineModelStatus.Online,
+                    HealthStatus = MachineModelHealthStatus.HasWarnings,
+                    EnvironmentIds = new ReferenceCollection("Environments-001")
+                },
+                new MachineResource {
+                    Name = "PC01996",
+                    Id = "Machines-003",
+                    Status = MachineModelStatus.Offline,
+                    HealthStatus = MachineModelHealthStatus.Healthy,
+                    EnvironmentIds = new ReferenceCollection("Environments-001")}
+            });
+
+            listMachinesCommand.Execute(CommandLineArgs.ToArray());
+
+            Log.Received().Info("Machines: 1");
+            Log.Received().InfoFormat(MachineLogFormat, "PC01466", MachineModelHealthStatus.HasWarnings.ToString(), "Machines-002", "Development");
+        }
+
+        [Test]
+        public void ShouldLogWarningIfUsingStatusOn34Repo()
+        {
+            CommandLineArgs.Add("--status=Online");
+            Repository.Client.RootDocument.Version = "3.4.0";
+            Repository.Environments.FindAll().Returns(new List<EnvironmentResource>
+            {
+                new EnvironmentResource {Name = "Development", Id = "Environments-001"}
+            });
+
+            Repository.Machines.FindAll().Returns(new List<MachineResource>
+            {
+                new MachineResource {
+                    Name = "PC0123",
+                    Id = "Machines-001",
+                    Status = MachineModelStatus.Online,
+                    HealthStatus = MachineModelHealthStatus.Unavailable,
+                    EnvironmentIds = new ReferenceCollection("Environments-001")
+                },
+                new MachineResource {
+                    Name = "PC01466",
+                    Id = "Machines-002",
+                    Status = MachineModelStatus.Online,
+                    HealthStatus = MachineModelHealthStatus.HasWarnings,
+                    EnvironmentIds = new ReferenceCollection("Environments-001")
+                },
+                new MachineResource {
+                    Name = "PC01996",
+                    Id = "Machines-003",
+                    Status = MachineModelStatus.Offline,
+                    HealthStatus = MachineModelHealthStatus.Healthy,
+                    EnvironmentIds = new ReferenceCollection("Environments-001")}
+            });
+
+            listMachinesCommand.Execute(CommandLineArgs.ToArray());
+
+            Log.Received().Warn("The `--status` parameter will be depricated in Octopus Deploy 4.0. You may want to execute this command with the `--health-status=` parameter instead.");
+            Log.Received().Info("Machines: 2");
+        }
+
+        [Test]
+        public void ShouldSupportStateFilters()
+        {
+            CommandLineArgs.Add("--health-status=Healthy");
+            CommandLineArgs.Add("--calamari-outdated=false");
+            CommandLineArgs.Add("--tentacle-outdated=true");
+            CommandLineArgs.Add("--disabled=true");
+            Repository.Client.RootDocument.Version = "3.4.0";
+            Repository.Environments.FindAll().Returns(new List<EnvironmentResource>
+            {
+                new EnvironmentResource {Name = "Development", Id = "Environments-001"}
+            });
+
+            Repository.Machines.FindAll().Returns(new List<MachineResource>
+            {
+                new MachineResource {
+                    Name = "PC0123",
+                    Id = "Machines-001",
+                    HealthStatus = MachineModelHealthStatus.Unavailable,
+                    EnvironmentIds = new ReferenceCollection("Environments-001")
+                },
+                new MachineResource {
+                    Name = "PC01466",
+                    Id = "Machines-002",
+                    HealthStatus = MachineModelHealthStatus.Healthy,
+                    IsDisabled = true,
+                    HasLatestCalamari = true,
+                    Endpoint = new ListeningTentacleEndpointResource() {TentacleVersionDetails = new TentacleDetailsResource { UpgradeSuggested = true } },
+                    EnvironmentIds = new ReferenceCollection("Environments-001")
+                },
+                new MachineResource {
+                    Name = "PC01467",
+                    Id = "Machines-003",
+                    HealthStatus = MachineModelHealthStatus.Healthy,
+                    IsDisabled = true,
+                    HasLatestCalamari = true,
+                    Endpoint = new ListeningTentacleEndpointResource() {TentacleVersionDetails = new TentacleDetailsResource { UpgradeSuggested = false } },
+                    EnvironmentIds = new ReferenceCollection("Environments-001")
+                },
+                new MachineResource {
+                    Name = "PC01468",
+                    Id = "Machines-004",
+                    HealthStatus = MachineModelHealthStatus.Healthy,
+                    IsDisabled = true,
+                    EnvironmentIds = new ReferenceCollection("Environments-001"),
+                    HasLatestCalamari = false
+                },
+                new MachineResource {
+                    Name = "PC01999",
+                    Id = "Machines-005",
+                    HealthStatus = MachineModelHealthStatus.Healthy,
+                    IsDisabled = false,
+                    EnvironmentIds = new ReferenceCollection("Environments-001")}
+            });
+
+            listMachinesCommand.Execute(CommandLineArgs.ToArray());
+
+            Log.Received().Info("Machines: 1");
+            Log.Received().InfoFormat(MachineLogFormat, "PC01466", "Healthy - Disabled", "Machines-002", "Development");
+        }
+
+        [Test, ExpectedException(typeof(CommandException), ExpectedMessage = "The `--health-status` parameter is only available on Octopus Server instances from 3.4.0 onwards.")]
+        public void ShouldThrowIfUsingHealthStatusPre34()
+        {
+            CommandLineArgs.Add("--health-status=Online");
+            Repository.Client.RootDocument.Version = "3.1.0";
+
+            listMachinesCommand.Execute(CommandLineArgs.ToArray());
         }
 
         [Test]
