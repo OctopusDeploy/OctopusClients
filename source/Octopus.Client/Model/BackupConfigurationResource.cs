9b34ee39fda3b936d7853c81190be61acbb64981
Robert Wagner <robert.wagner@octopus.com>
2016-08-30 21:25:04 +1000

    Fixed gitIgnore

---
+++ source/Octopus.Client/Model/BackupConfigurationResource.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Octopus.Client.Model
+{
+    public class BackupConfigurationResource : Resource
+    {
+        [Writeable]
+        public string BackupToDirectory { get; set; }
+
+        [Writeable]
+        public bool IsMasterKeyBackedUp { get; set; }
+        
+        [Writeable]
+        public bool BackupAutomatically { get; set; }
+
+        [Writeable]
+        public TimeSpan? BackupEvery { get; set; }
+
+        [Writeable]
+        public TimeSpan? StartingFrom { get; set; }
+
+        [Writeable]
+        public RetentionPeriod Retention { get; set; }
+
+        public DateTimeOffset? NextDue { get; set; }
+    }
+}
