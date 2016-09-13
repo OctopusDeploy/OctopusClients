045ac849f9888822c0d1c2fee9b4bec1c6070265
Pawel Pabich <pawel@pabich.eu>
2016-09-13 11:42:23 +1000

    Added missing resource to complete the client move

---
+++ source/Octopus.Client/Model/StepResource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Octopus.Client.Model
+{
+    public class StepResource : IResource
+    {
+        public string Id { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string IconUrl { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+
+        public LinkCollection Links { get; set; }
+    }
+}
