d95388a1b5298f0e3792087176ca3651bd6cf779
robert <robert.erez@gmail.com>
2016-09-14 14:13:09 +1000

    Split FeedResource into DockerFeedResource and NuGetFeedResource while still allowing FeedResource for pre-docker server instances

---
+++ source/Octopus.Client/Serialization/FeedConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Octopus.Client.Model;
+
+namespace Octopus.Client.Serialization
+{
+    public class FeedConverter : InheritedClassConverter<FeedResource, FeedType>
+    {
+        static readonly IDictionary<FeedType, Type> FeedTypeMappings =
+            new Dictionary<FeedType, Type>
+            {
+                {FeedType.NuGet, typeof(NuGetFeedResource)},
+                {FeedType.Docker, typeof(DockerFeedResource)}
+            };
+
+        protected override IDictionary<FeedType, Type> DerivedTypeMappings => FeedTypeMappings;
+        protected override string TypeDesignatingPropertyName => "FeedType";
+    }
+}
