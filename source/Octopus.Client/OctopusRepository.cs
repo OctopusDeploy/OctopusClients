eb2900f4206ce22c5fc05af2b23f92262ba84d65
Mark Siedle <mark.siedle@octopus.com>
2016-09-23 12:21:48 +1000

    Updates to the event repositories for api tests

--- source/Octopus.Client/OctopusRepository.cs
+++ source/Octopus.Client/OctopusRepository.cs
@@ -984,9 +984,32 @@ namespace Octopus.Client
             {
             }
 
-            public ResourceCollection<EventResource> List(int skip = 0, string filterByUserId = null, string regardingDocumentId = null, bool includeInternalEvents = false)
+            public ResourceCollection<EventResource> List(int skip = 0,
+                string from = null,
+                string to = null,
+                string regarding = null,
+                string regardingAny = null,
+                string users = null,
+                string projects = null,
+                string environments = null,
+                string eventGroups = null,
+                string eventCategories = null,
+                string tenants = null,
+                string tags = null)
             {
-                return Client.List<EventResource>(Client.RootDocument.Link("Events"), new {skip, user = filterByUserId, regarding = regardingDocumentId, @internal = includeInternalEvents.ToString()});
+                return Client.List<EventResource>(Client.RootDocument.Link("Events"), new {skip,
+                    from = from,
+                    to = to,
+                    regarding = regarding,
+                    regardingAny = regardingAny,
+                    users = users,
+                    projects = projects,
+                    environments = environments,
+                    eventGroups = eventGroups,
+                    eventCategories = eventCategories,
+                    tenants = tenants,
+                    tags = tags
+                });
             }
         }
 
--- source/Octopus.Client/Repositories/IEventRepository.cs
+++ source/Octopus.Client/Repositories/IEventRepository.cs
@@ -5,6 +5,17 @@ namespace Octopus.Client.Repositories
 {
     public interface IEventRepository : IGet<EventResource>
     {
-        ResourceCollection<EventResource> List(int skip = 0, string filterByUserId = null, string regardingDocumentId = null, bool includeInternalEvents = false);
+        ResourceCollection<EventResource> List(int skip = 0,
+                string from = null,
+                string to = null,
+                string regarding = null,
+                string regardingAny = null,
+                string users = null,
+                string projects = null,
+                string environments = null,
+                string eventGroups = null,
+                string eventCategories = null,
+                string tenants = null,
+                string tags = null);
     }
 }
