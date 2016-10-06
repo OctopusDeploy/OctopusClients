2ad4a5e7ed53964b5d83398e9a0e512cc9d33088
Pawel Pabich <pawel@pabich.eu>
2016-10-06 14:44:12 +1000

    Exposed link to template history

--- source/Octopus.Client/Model/StepTemplateResource.cs
+++ source/Octopus.Client/Model/StepTemplateResource.cs
@@ -15,6 +15,7 @@ namespace Octopus.Client.Model
 
         public string LibraryStepTemplateId { get; set; }
         public bool HasUpdate { get; set; }
+        public string HistoryUrl { get; set; }
 
         public LinkCollection Links { get; set; }
     }
