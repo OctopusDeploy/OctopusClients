2ee8ec0c91632e4a4411472ae87b83c83b18da2b
Rob Pearson <robpearson@fastmail.net>
2016-10-10 11:12:45 +1000

    Added  community step tempaltes feature toggle.

--- source/Octopus.Client/Model/FeaturesConfigurationResource.cs
+++ source/Octopus.Client/Model/FeaturesConfigurationResource.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Octopus.Client.Model
+﻿namespace Octopus.Client.Model
 {
     public class FeaturesConfigurationResource : Resource
     {
         [Writeable]
         public bool IsMultiTenancyEnabled { get; set; }
+        [Writeable]
+        public bool IsCommunityStepTemplatesEnabled { get; set; }
     }
 }
