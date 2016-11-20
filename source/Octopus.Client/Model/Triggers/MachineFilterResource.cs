f4a44813228dc5cc9c5bc086c0a3d07cfbf8b27d
Shane Gill <tothegills@gmail.com>
2016-11-21 09:47:36 +1000

    Rename resource

--- source/Octopus.Client/Model/Triggers/MachineFilterResource.cs
+++ source/Octopus.Client/Model/Triggers/MachineFilterResource.cs
@@ -1,12 +1,11 @@
 ﻿namespace Octopus.Client.Model.Triggers
 {
-    public class TriggerMachineFilterResource : TriggerFilterResource
+    public class MachineFilterResource : TriggerFilterResource
     {
         public override TriggerFilterType FilterType => TriggerFilterType.MachineFilter;
         public ReferenceCollection EnvironmentIds { get; } = new ReferenceCollection();
         public ReferenceCollection Roles { get; } = new ReferenceCollection();
         public ReferenceCollection EventGroups { get; } = new ReferenceCollection();
         public ReferenceCollection EventCategories { get; } = new ReferenceCollection();
-        
     }
 }
