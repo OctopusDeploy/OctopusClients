132309d96a2c4fc03f2546a4190203b01659344b
Shane Gill <tothegills@gmail.com>
2016-11-24 12:27:21 +1000

    Only show latest deployments with show-latestdeployments command Fixes OctopusDeploy/Issues#2992

--- source/Octo/Commands/ListLatestDeploymentsCommand.cs
+++ source/Octo/Commands/ListLatestDeploymentsCommand.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using NuGet.Packaging;
 using Octopus.Cli.Infrastructure;
 using Octopus.Cli.Repositories;
 using Octopus.Cli.Util;
@@ -29,37 +28,75 @@ namespace Octopus.Cli.Commands
         protected override async Task Execute()
         {
             var projectsFilter = new string[0];
+            var projectsById = new Dictionary<string, string>();
             if (projects.Count > 0)
             {
                 Log.Debug("Loading projects...");
                 var projectResources = await Repository.Projects.FindByNames(projects.ToArray()).ConfigureAwait(false);
                 projectsFilter = projectResources.Select(p => p.Id).ToArray();
+                projectsById = projectResources.ToDictionary(p => p.Id, p => p.Name);
+            }
+            else
+            {
+                projectsById = (await Repository.Projects.FindAll().ConfigureAwait(false)).ToDictionary(p => p.Id, p => p.Name);
             }
 
+            var environmentsById = await LoadEnvironments();
+
+            Log.Debug("Loading dashboard...");
+            var dashboardItems =
+                await
+                    Repository.Dashboards.GetDynamicDashboard(projectsFilter,
+                            environments.Count > 0 ? environmentsById.Keys.ToArray() : new string[] {})
+                        .ConfigureAwait(false);
+
+            Log.Debug("Loading deployments...");
+            var deployments =
+                await Repository.Deployments.Get(dashboardItems.Items.Select(d => d.DeploymentId).ToArray()).ConfigureAwait(false);
+
+            var deploymentsById = deployments.ToDictionary(p => p.Id, p => p);
+
+            foreach (var item in dashboardItems.Items)
+            {
+                await LogDeploymentInfo(deploymentsById[item.DeploymentId], environmentsById, projectsById).ConfigureAwait(false);
+            }
+        }
+
+        async Task<IDictionary<string, string>> LoadProjects()
+        {
+            Log.Debug("Loading projects...");
+
+            if (projects.Any())
+            {
+                var projectResources = Repository.Projects.FindByNames(projects.ToArray()).ConfigureAwait(false);
+                return (await projectResources).ToDictionary(p => p.Id, p => p.Name);
+            }
+
+            var allProjects = Repository.Projects.FindAll().ConfigureAwait(false);
+            return (await allProjects).ToDictionary(p => p.Id, p => p.Name);
+        }
+
+        async Task<IDictionary<string, string>> LoadEnvironments()
+        {
             Log.Debug("Loading environments...");
             var environmentResources = environments.Count > 0
                 ? Repository.Environments.FindByNames(environments.ToArray())
                 : Repository.Environments.FindAll();
 
-            var environmentsById = (await environmentResources.ConfigureAwait(false)).ToDictionary(p => p.Id, p => p.Name);
-
-            var deployments = await Repository.Deployments.FindAll(projectsFilter, environments.Count > 0 ? environmentsById.Keys.ToArray() : new string[] { }).ConfigureAwait(false);
-
-            foreach (var deployment in deployments.Items)
-            {
-                await LogDeploymentInfo(deployment, environmentsById).ConfigureAwait(false);
-            }
+            return (await environmentResources.ConfigureAwait(false)).ToDictionary(p => p.Id, p => p.Name);
         }
 
-        public async Task LogDeploymentInfo(DeploymentResource deployment, Dictionary<string, string> environmentsById)
+        public async Task LogDeploymentInfo(DeploymentResource deployment, IDictionary<string, string> environmentsById, IDictionary<string, string> projectedById)
         {
             var nameOfDeploymentEnvironment = environmentsById[deployment.EnvironmentId];
+            var nameOfDeploymentProject = projectedById[deployment.ProjectId];
             var task = Repository.Tasks.Get(deployment.Link("Task")).ConfigureAwait(false);
             var release = Repository.Releases.Get(deployment.Link("Release")).ConfigureAwait(false);
 
             var propertiesToLog = new List<string>();
             propertiesToLog.AddRange(FormatTaskPropertiesAsStrings(await task));
             propertiesToLog.AddRange(FormatReleasePropertiesAsStrings(await release));
+            Log.Information(" - Project: {Project:l}", nameOfDeploymentProject);
             Log.Information(" - Environment: {Environment:l}", nameOfDeploymentEnvironment);
             foreach (var property in propertiesToLog)
             {
