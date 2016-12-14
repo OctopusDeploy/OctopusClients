440bdd3fbd643e5407c45edccfa3c6ae0775f6bc
Robert Wagner <robert.wagner@octopus.com>
2016-12-15 07:58:05 +1000

    Refactored to merge some interfaces into their implementations

--- source/Octopus.Client/Repositories/Async/ActionTemplateRepository.cs
+++ source/Octopus.Client/Repositories/Async/ActionTemplateRepository.cs
@@ -4,6 +4,11 @@ using Octopus.Client.Model;
 
 namespace Octopus.Client.Repositories.Async
 {
+    public interface IActionTemplateRepository : ICreate<ActionTemplateResource>, IModify<ActionTemplateResource>, IDelete<ActionTemplateResource>, IGet<ActionTemplateResource>, IFindByName<ActionTemplateResource>, IGetAll<ActionTemplateResource>
+    {
+        Task<List<ActionTemplateSearchResource>> Search();
+    }
+
     class ActionTemplateRepository : BasicRepository<ActionTemplateResource>, IActionTemplateRepository
     {
         public ActionTemplateRepository(IOctopusAsyncClient client) : base(client, "ActionTemplates")
--- source/Octopus.Client/Repositories/Async/ProxyRepository.cs
+++ source/Octopus.Client/Repositories/Async/ProxyRepository.cs
@@ -3,6 +3,10 @@ using Octopus.Client.Model;
 
 namespace Octopus.Client.Repositories.Async
 {
+    public interface IProxyRepository : IGet<ProxyResource>, ICreate<ProxyResource>, IModify<ProxyResource>, IDelete<ProxyResource>, IFindByName<ProxyResource>
+    {
+    }
+
     class ProxyRepository : BasicRepository<ProxyResource>, IProxyRepository
     {
         public ProxyRepository(IOctopusAsyncClient client)
