fd62d8da31f8f5d59728d32b4c349716c9536824
Michael Noonan <michaelnoonan@gmail.com>
2017-02-06 21:01:39 +1000

    Added extra ApiConstants for CSRF prevention.

--- source/Octopus.Client/Model/ApiConstants.cs
+++ source/Octopus.Client/Model/ApiConstants.cs
@@ -4,9 +4,12 @@ namespace Octopus.Client.Model
 {
     public class ApiConstants
     {
-        public const int DefaultClientRequestTimeout = 1000*60*10;
-        public const string SupportedApiSchemaVersionMin = "3.0.0";
-        public const string SupportedApiSchemaVersionMax = "3.0.99";
-        public const string ApiKeyHttpHeaderName = "X-Octopus-ApiKey";
+        public static readonly int DefaultClientRequestTimeout = 1000 * 60 * 10;
+        public static readonly string SupportedApiSchemaVersionMin = "3.0.0";
+        public static readonly string SupportedApiSchemaVersionMax = "3.0.99";
+        public static readonly string AuthenticationCookiePrefix = "OctopusIdentificationToken";
+        public static readonly string ApiKeyHttpHeaderName = "X-Octopus-ApiKey";
+        public static readonly string AntiforgeryTokenCookiePrefix = "Octopus-Csrf-Token";
+        public static readonly string AntiforgeryTokenHttpHeaderName = "X-Octopus-Csrf-Token";
     }
 }
