48913c76991982cd91ee702d1988aafc38cc388b
Robert Wagner <robert@wagner.id.au>
2017-02-22 13:54:12 +1000

    Fixes #131. When we changed `TaskDetails.ActivityLog` to `ActivityLogs` (3.4.0ish) we no longer checked the progress of the children (ie each steps). Instead only checked the overall process. Therefore the log was only printed once the whole task has finished instead of when each step finished.

---
+++ source/Octopus.Client.Tests/Repositories/Async/TaskRepositoryTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using Octopus.Client.Model;
+using Octopus.Client.Repositories.Async;
+
+namespace Octopus.Client.Tests.Repositories.Async
+{
+    public class TaskRepositoryTests
+    {
+
+        [Test]
+        public void WaitForCompletionReportsProgress_ActionOverload()
+        {
+            var client = Substitute.For<IOctopusAsyncClient>();
+            var repository = new TaskRepository(client);
+            var taskResource = new TaskResource {Links = new LinkCollection() {{"Self", ""}}, State = TaskState.Queued};
+
+            client.Get<TaskResource>(Arg.Any<string>()).Returns(c => Task.FromResult(taskResource));
+
+            var callbackCount = 0;
+
+            Action<TaskResource[]> action = t =>
+            {
+                t[0].Should().Be(taskResource);
+                callbackCount++;
+                taskResource = new TaskResource()
+                {
+                    State = callbackCount > 3 ? TaskState.Success : TaskState.Executing
+                };
+            };
+
+
+            var wait = repository.WaitForCompletion(taskResource, pollIntervalSeconds: 1, timeoutAfterMinutes:1, interval: action);
+            wait.Wait(TimeSpan.FromSeconds(30));
+
+            callbackCount.Should().BeGreaterThan(3);
+        }
+
+        [Test]
+        public void WaitForCompletionReportsProgress_TaskOverload()
+        {
+            var client = Substitute.For<IOctopusAsyncClient>();
+            var repository = new TaskRepository(client);
+            var taskResource = new TaskResource {Links = new LinkCollection() {{"Self", ""}}, State = TaskState.Queued };
+
+            client.Get<TaskResource>(Arg.Any<string>()).Returns(c => Task.FromResult(taskResource));
+
+            var callbackCount = 0;
+
+            Func<TaskResource[], Task> func = t => Task.Run(() =>
+            {
+                t[0].Should().Be(taskResource);
+                callbackCount++;
+                taskResource = new TaskResource()
+                {
+                    State = callbackCount > 3 ? TaskState.Success : TaskState.Executing
+                };
+            });
+
+
+            var wait = repository.WaitForCompletion(new [] { taskResource }, pollIntervalSeconds: 1, timeoutAfterMinutes: 1, interval: func);
+            wait.Wait(TimeSpan.FromSeconds(30));
+
+            callbackCount.Should().BeGreaterThan(3);
+        }
+    }
+}
--- source/Octopus.Client/Model/TaskDetailsResource.cs
+++ source/Octopus.Client/Model/TaskDetailsResource.cs
@@ -1,11 +1,34 @@
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace Octopus.Client.Model
 {
     public class TaskDetailsResource : Resource
     {
+        private ActivityElement activityLog;
+
         public TaskResource Task { get; set; }
+
         public IList<ActivityElement> ActivityLogs { get; set; }
+
+        /// <summary>
+        /// Returned by Pre 3.4 servers
+        /// Sets the ActivityLogs property as well if it does not already have a value
+        /// </summary>
+        [Obsolete("Use ActivityLogs property")]
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public ActivityElement ActivityLog
+        {
+            get { return activityLog; }
+            set
+            {
+                activityLog = value;
+                if (ActivityLogs == null && activityLog != null)
+                    ActivityLogs = new List<ActivityElement>() {activityLog};
+            }
+        }
+
         public TaskProgress Progress { get; set; }
     }
 }
