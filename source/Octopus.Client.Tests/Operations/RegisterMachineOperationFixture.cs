9fb5cd1572cdf1dacfe7773c97c0dd765510200d
Shane Gill <tothegills@gmail.com>
2017-03-09 12:28:21 +1000

    Missed a space

--- source/Octopus.Client.Tests/Operations/RegisterMachineOperationFixture.cs
+++ source/Octopus.Client.Tests/Operations/RegisterMachineOperationFixture.cs
@@ -58,7 +58,7 @@ namespace Octopus.Client.Tests.Operations
 
             operation.EnvironmentNames = new[] {"Production", "Atlantis", "Hyperborea"};
             Func<Task> exec = () => operation.ExecuteAsync(serverEndpoint);
-            exec.ShouldThrow<ArgumentException>().WithMessage("Could not find the environments named: Atlantis, Hyperborea on the Octopus server.Ensure the environments exist and you have permission to access them.");
+            exec.ShouldThrow<ArgumentException>().WithMessage("Could not find the environments named: Atlantis, Hyperborea on the Octopus server. Ensure the environments exist and you have permission to access them.");
         }
 
         [Test]
