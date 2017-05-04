1b2131e3054e37e9101702e42cdc0d2d9b35b7f5
Robert <robert.erez@gmail.com>
2017-05-04 13:07:58 +1000

    Fix Tests#3

--- source/Octopus.Client.Tests/PublicSurfaceAreaFixture.cs
+++ source/Octopus.Client.Tests/PublicSurfaceAreaFixture.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Assent;
 using Assent.Namers;
