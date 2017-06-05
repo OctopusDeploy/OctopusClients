5f0a49791b05258b76e545dd4682b84f8c0625c7
Robert Wagner <robert.wagner@octopus.com>
2017-06-05 16:35:53 +1000

    Convert to new project system

--- source/Octopus.Client.Tests/Integration/OctopusClient/StatusTests.cs
+++ source/Octopus.Client.Tests/Integration/OctopusClient/StatusTests.cs
@@ -2,7 +2,6 @@ using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using FluentAssertions;
-using Microsoft.DotNet.Tools.Test;
 using Nancy;
 using NUnit.Framework;
 using Octopus.Client.Exceptions;
