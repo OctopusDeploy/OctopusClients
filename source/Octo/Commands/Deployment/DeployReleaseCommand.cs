68708bc77cf80ce430be77a5359769507ec59139
Ben Pearce <ben.pearce@octopusdeploy.com>
2017-08-24 18:31:51 +1000

    Added JSON support to Deploy Release command

--- source/Octo/Commands/Deployment/DeployReleaseCommand.cs
+++ source/Octo/Commands/Deployment/DeployReleaseCommand.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Octopus.Cli.Infrastructure;
 using Octopus.Cli.Repositories;
@@ -10,8 +11,12 @@ using Serilog;
 namespace Octopus.Cli.Commands.Deployment
 {
     [Command("deploy-release", Description = "Deploys a release.")]
-    public class DeployReleaseCommand : DeploymentCommandBase
+    public class DeployReleaseCommand : DeploymentCommandBase, ISupportFormattedOutput
     {
+        ProjectResource project;
+        ChannelResource channel;
+        ReleaseResource releaseToPromote;
+
         public DeployReleaseCommand(IOctopusAsyncRepositoryFactory repositoryFactory, ILogger log, IOctopusFileSystem fileSystem, IOctopusClientFactory clientFactory, ICommandOutputProvider commandOutputProvider)
             : base(repositoryFactory, log, fileSystem, clientFactory, commandOutputProvider)
         {
@@ -37,15 +42,15 @@ namespace Octopus.Cli.Commands.Deployment
             base.ValidateParameters();
         }
 
-        protected override async Task Execute()
+        public async Task Request()
         {
-            var project = await RepositoryCommonQueries.GetProjectByName(ProjectName).ConfigureAwait(false);
-            var channel = await GetChannel(project).ConfigureAwait(false);
-            var releaseToPromote = await RepositoryCommonQueries.GetReleaseByVersion(VersionNumber, project, channel).ConfigureAwait(false);
+            project = await RepositoryCommonQueries.GetProjectByName(ProjectName).ConfigureAwait(false);
+            channel = await GetChannel(project).ConfigureAwait(false);
+            releaseToPromote = await RepositoryCommonQueries.GetReleaseByVersion(VersionNumber, project, channel).ConfigureAwait(false);
 
             if (UpdateVariableSnapshot)
             {
-                Log.Debug("Updating the release variable snapshot with variables from the project");
+                commandOutputProvider.Debug("Updating the release variable snapshot with variables from the project");
                 await Repository.Releases.SnapshotVariables(releaseToPromote);
             }
 
@@ -57,7 +62,7 @@ namespace Octopus.Cli.Commands.Deployment
             var channel = default(ChannelResource);
             if (!string.IsNullOrWhiteSpace(ChannelName))
             {
-                Log.Debug("Finding channel: {Channel:l}", ChannelName);
+                commandOutputProvider.Debug("Finding channel: {Channel:l}", ChannelName);
                 var channels = await Repository.Projects.GetChannels(project).ConfigureAwait(false);
                 channel = await channels
                     .FindOne(Repository, c => string.Equals(c.Name, ChannelName, StringComparison.OrdinalIgnoreCase)).ConfigureAwait(false);
@@ -66,6 +71,47 @@ namespace Octopus.Cli.Commands.Deployment
                     throw new CouldNotFindException("a channel named", ChannelName);
             }
             return channel;
+        }
+
+        public void PrintDefaultOutput()
+        {
+            
+        }
+
+        public void PrintJsonOutput()
+        {
+            //commandOutputProvider.Json(project);
+            //commandOutputProvider.Json(channel);
+            //commandOutputProvider.Json(releaseToPromote);
+            commandOutputProvider.Json(new
+            {
+                ProjectName = project.Name,
+                releaseToPromote.Version,
+                Deployments = deployments.Select(d => new
+                {
+                    DeploymentId = d.Id,
+                    d.ReleaseId,
+                    Environment = new
+                    {
+                        d.EnvironmentId,
+                        EnvironmentName = promotionTargets.FirstOrDefault(x => x.Id == d.EnvironmentId)?.Name
+                    },
+                    d.SkipActions,
+                    d.SpecificMachineIds,
+                    d.ExcludedMachineIds,
+                    d.Created,
+                    d.Name,
+                    d.QueueTime,
+                    Tenant = string.IsNullOrEmpty(d.TenantId)
+                        ? null
+                        : new { d.TenantId, TenantName = deploymentTenants.FirstOrDefault(x => x.Id == d.TenantId)?.Name }
+                })
+            });
+        }
+
+        public void PrintXmlOutput()
+        {
+            throw new NotImplementedException();
         }
     }
 }
