a5f9403fd0540eb20e48f1679466c2f8d9bb2ca9
Ben Pearce <ben.pearce@octopusdeploy.com>
2017-08-28 13:56:01 +1000

    Add JSON output to Pus command

--- source/Octo/Commands/Package/PushCommand.cs
+++ source/Octo/Commands/Package/PushCommand.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Octopus.Cli.Infrastructure;
+using Octopus.Cli.Model;
 using Octopus.Cli.Repositories;
 using Octopus.Cli.Util;
 using Octopus.Client;
@@ -11,20 +13,26 @@ using Serilog;
 namespace Octopus.Cli.Commands.Package
 {
     [Command("push", Description = "Pushes a package (.nupkg, .zip, .tar.gz, etc.) package to the built-in NuGet repository in an Octopus server.")]
-    public class PushCommand : ApiCommand
+    public class PushCommand : ApiCommand, ISupportFormattedOutput
     {
+        private List<string> pushedPackages;
+        private List<Tuple<string, Exception>> failedPackages;
+
         public PushCommand(IOctopusAsyncRepositoryFactory repositoryFactory, IOctopusFileSystem fileSystem, IOctopusClientFactory clientFactory, ICommandOutputProvider commandOutputProvider)
             : base(clientFactory, repositoryFactory, fileSystem, commandOutputProvider)
         {
             var options = Options.For("Package pushing");
             options.Add("package=", "Package file to push. Specify multiple packages by specifying this argument multiple times: \n--package package1 --package package2", package => Packages.Add(EnsurePackageExists(fileSystem, package)));
             options.Add("replace-existing", "If the package already exists in the repository, the default behavior is to reject the new package being pushed. You can pass this flag to overwrite the existing package.", replace => ReplaceExisting = true);
+
+            pushedPackages = new List<string>();
+            failedPackages = new List<Tuple<string, Exception>>();
         }
 
         public HashSet<string> Packages { get; } = new HashSet<string>(StringComparer.OrdinalIgnoreCase); 
         public bool ReplaceExisting { get; set; }
 
-        protected override async Task Execute()
+        public async Task Request()
         {
             if (Packages.Count == 0) throw new CommandException("Please specify a package to push");
 
@@ -32,13 +40,39 @@ namespace Octopus.Cli.Commands.Package
             {
                 commandOutputProvider.Debug("Pushing package: {Package:l}...", package);
 
-                using (var fileStream = FileSystem.OpenFile(package, FileAccess.Read))
+                try
                 {
-                    await Repository.BuiltInPackageRepository.PushPackage(Path.GetFileName(package), fileStream, ReplaceExisting).ConfigureAwait(false);
+                    using (var fileStream = FileSystem.OpenFile(package, FileAccess.Read))
+                    {
+                        await Repository.BuiltInPackageRepository
+                            .PushPackage(Path.GetFileName(package), fileStream, ReplaceExisting).ConfigureAwait(false);
+                    }
+
+                    pushedPackages.Add(package);
+                }
+                catch (Exception ex)
+                {
+                    if (OutputFormat == OutputFormat.Default)
+                    {
+                        throw;
+                    }
+                    failedPackages.Add(new Tuple<string, Exception>(package, ex));
                 }
             }
+        }
 
+        public void PrintDefaultOutput()
+        {
             commandOutputProvider.Debug("Push successful");
+        }
+
+        public void PrintJsonOutput()
+        {
+            commandOutputProvider.Json(new
+            {
+                SuccessfulPackages = pushedPackages,
+                FailedPackages = failedPackages.Select(x=>new { Package = x.Item1, Reason = x.Item2.Message.Replace("\r\n", string.Empty) })
+            });
         }
 
         static string EnsurePackageExists(IOctopusFileSystem fileSystem, string package)
