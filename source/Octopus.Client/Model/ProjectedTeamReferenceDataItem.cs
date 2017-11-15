ce369d933f4fe305d2b77cc1581adc6ea4d160a9
Shaun Marx <shaun.m4rx@gmail.com>
2017-11-16 08:48:28 +1000

    Update user permissions response

---
+++ source/Octopus.Client/Model/ProjectedTeamReferenceDataItem.cs
@@ -0,0 +1,14 @@
+﻿namespace Octopus.Client.Model
+{
+    public class ProjectedTeamReferenceDataItem : ReferenceDataItem
+    {
+        public bool IsDirectlyAssigned { get; set; }
+        public string[] ExternalGroupNames { get; set; }
+
+        public ProjectedTeamReferenceDataItem(string id, string name, bool isDirectlyAssigned, string[] externalGroupNames) : base(id, name)
+        {
+            ExternalGroupNames = externalGroupNames;
+            IsDirectlyAssigned = isDirectlyAssigned;
+        }
+    }
+}
