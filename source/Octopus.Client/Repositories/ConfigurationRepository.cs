318c997b94f8b3d6d19e4290b444920bd3d9f5d6
Ben Pearce <ben.pearce@octopus.com>
2017-11-21 11:51:51 +1000

    Cleanup

--- source/Octopus.Client/Repositories/ConfigurationRepository.cs
+++ source/Octopus.Client/Repositories/ConfigurationRepository.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Octopus.Client.Extensibility;
+﻿using Octopus.Client.Extensibility;
 using Octopus.Client.Model;
 
 namespace Octopus.Client.Repositories
