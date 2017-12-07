7e7b65e8adc895edac2d9f3317b28dc939878bbf
Shane Gill <tothegills@gmail.com>
2017-12-07 10:37:36 +1000

    Add PackageRequirement to DeploymentStep Relates to OctopusDeploy/Issues#3974

---
+++ source/Octopus.Client/Model/DeploymentStepPackageRequirement.cs
@@ -0,0 +1,9 @@
+﻿namespace Octopus.Client.Model
+{
+    public enum DeploymentStepPackageRequirement
+    {
+        LetOctopusDecide,
+        BeforePackageAcquisition,
+        AfterPackageAcquisition
+    }
+}
