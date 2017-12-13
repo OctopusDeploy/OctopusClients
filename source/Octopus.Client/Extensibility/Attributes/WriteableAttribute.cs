3836c7a12c3be9e0115bd4ab6ebac7b380e47f31
Shannon Lewis <slewis74@gmail.com>
2017-12-13 14:41:56 +1000

    removed the extensibility package and moved the code back in here

---
+++ source/Octopus.Client/Extensibility/Attributes/WriteableAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Octopus.Client.Extensibility.Attributes
+{
+    /// <summary>
+    /// Properties with this attribute will be persisted to the server when sent using a POST or PUT request.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class WriteableAttribute : ApiPropertyAttribute
+    {
+    }
+
+    /// <summary>
+    /// Properties with this attribute will be persisted to the server when sent using a POST request.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class WriteableOnCreateAttribute : ApiPropertyAttribute
+    {
+    }
+
+}
