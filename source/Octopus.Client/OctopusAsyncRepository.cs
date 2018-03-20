8e0526b3d25c94ec1ce80ac4a8282af9325ec390
Nick Josevski <nick@josevski.com>
2018-03-20 16:09:25 +1000

    teams v2 repository

--- source/Octopus.Client/OctopusAsyncRepository.cs
+++ source/Octopus.Client/OctopusAsyncRepository.cs
@@ -69,6 +69,7 @@ namespace Octopus.Client
             TagSets = new TagSetRepository(client);
             Tasks = new TaskRepository(client);
             Teams = new TeamsRepository(client);
+            TeamsV2 = new TeamsV2Repository(client);
             Tenants = new TenantRepository(client);
             TenantVariables = new TenantVariablesRepository(client);
             UserRoles = new UserRolesRepository(client);
@@ -117,6 +118,7 @@ namespace Octopus.Client
         public ITagSetRepository TagSets { get; }
         public ITaskRepository Tasks { get; }
         public ITeamsRepository Teams { get; }
+        public ITeamsV2Repository TeamsV2 { get; }
         public ITenantRepository Tenants { get; }
         public ITenantVariablesRepository TenantVariables { get; }
         public IUserRepository Users { get; }
