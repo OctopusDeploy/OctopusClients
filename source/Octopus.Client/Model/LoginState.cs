a718b26cea9a5caf07a10cfccc1a06dcea33c068
Shannon Lewis <slewis74@gmail.com>
2018-03-28 13:45:10 +1000

    actually, client doesn't need to worry about RedirectAfterLoginTo

--- source/Octopus.Client/Model/LoginState.cs
+++ source/Octopus.Client/Model/LoginState.cs
@@ -3,11 +3,6 @@
     public class LoginState
     {
         /// <summary>
-        /// The Url, relative to the portal site, to redirect to post successful login.
-        /// </summary>
-        public string RedirectAfterLoginTo { get; set; }
-
-        /// <summary>
         /// Whether the client says it's using a secure connection. We need this because SSL offloading
         /// can obscure this and the server cannot tell whether the client initiated the call using a secure
         /// connection.
