e9d9f17aef7960f486aae7d7a3e00c4519957101
Shannon Lewis <slewis74@gmail.com>
2018-03-28 11:58:42 +1000

    new LoginState for secure cookie support when using SSL offloading

---
+++ source/Octopus.Client/Model/LoginState.cs
@@ -0,0 +1,17 @@
+﻿namespace Octopus.Client.Model
+{
+    public class LoginState
+    {
+        /// <summary>
+        /// The Url, relative to the portal site, to redirect to post successful login.
+        /// </summary>
+        public string RedirectAfterLoginTo { get; set; }
+
+        /// <summary>
+        /// Whether the client says it's using a secure connection. We need this because SSL offloading
+        /// can obscure this and the server cannot tell whether the client initiated the call using a secure
+        /// connection.
+        /// </summary>
+        public bool UsingSecureConnection { get; set; }
+    }
+}
