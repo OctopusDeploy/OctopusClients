27b2b6c37096e26aa103751eb7a3dca72ab9a3bd
Michael Compton <michael.compton@octopus.com>
2018-04-06 10:15:14 +1000

    Add Worker resources

--- source/Octopus.Client/Model/MachineResource.cs
+++ source/Octopus.Client/Model/MachineResource.cs
@@ -2,7 +2,6 @@
 
 namespace Octopus.Client.Model
 {
-    [Obsolete("MachineResource was deprecated in Octopus 2018.5.0.  Please use DeploymentTargetResource instead.")]
     public class MachineResource : DeploymentTargetResource
     {
         public MachineResource() : base()
