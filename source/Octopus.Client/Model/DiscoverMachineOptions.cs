b2ec352d47de72967f1eb3f97c824058c70fb655
Robert Wagner <robert.wagner@octopus.com>
2018-04-18 21:18:58 +1000

    Added support for specifying the proxy on the Discover call. Extra method added to avoid breaking API change, the Options object should ensure new parameters are easier to add. Fixes https://github.com/OctopusDeploy/OctopusClients/issues/244

---
+++ source/Octopus.Client/Model/DiscoverMachineOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace Octopus.Client.Model
+{
+    public class DiscoverMachineOptions
+    {
+        public DiscoverMachineOptions(string host)
+        {
+            Host = host;
+        }
+        
+        public string Host { get; }
+        public int Port { get; set; } = 10933;
+        public DiscoverableEndpointType? Type { get; set; }
+        public ProxyResource Proxy { get; set; }
+    }
+}
--- source/Octopus.Client/Repositories/Async/MachineRepository.cs
+++ source/Octopus.Client/Repositories/Async/MachineRepository.cs
@@ -10,6 +10,7 @@ namespace Octopus.Client.Repositories.Async
     public interface IMachineRepository : IFindByName<MachineResource>, IGet<MachineResource>, ICreate<MachineResource>, IModify<MachineResource>, IDelete<MachineResource>
     {
         Task<MachineResource> Discover(string host, int port = 10933, DiscoverableEndpointType? discoverableEndpointType = null);
+        Task<MachineResource> Discover(DiscoverMachineOptions options);
         Task<MachineConnectionStatus> GetConnectionStatus(MachineResource machine);
         Task<List<MachineResource>> FindByThumbprint(string thumbprint);
         Task<IReadOnlyList<TaskResource>> GetTasks(MachineResource machine);
@@ -51,9 +52,20 @@ namespace Octopus.Client.Repositories.Async
         }
 
         public Task<MachineResource> Discover(string host, int port = 10933, DiscoverableEndpointType? type = null)
-        {
-            return Client.Get<MachineResource>(Client.RootDocument.Link("DiscoverMachine"), new { host, port, type });
-        }
+            => Discover(new DiscoverMachineOptions(host)
+            {
+                Port = port,
+                Type = type
+            });
+
+        public Task<MachineResource> Discover(DiscoverMachineOptions options)
+            => Client.Get<MachineResource>(Client.RootDocument.Link("DiscoverMachine"), new
+            {
+                host = options.Host,
+                port = options.Port,
+                type = options.Type,
+                proxyId = options.Proxy?.Id
+            });
 
         public Task<MachineConnectionStatus> GetConnectionStatus(MachineResource machine)
         {
