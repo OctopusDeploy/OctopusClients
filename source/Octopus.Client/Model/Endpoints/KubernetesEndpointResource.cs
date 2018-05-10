071ebebbc0f4a42a317f762b0b168b530d61a794
Matthew Casperson <matthewcasperson@gmail.com>
2018-05-11 08:01:48 +1000

    Aded kubernetes targets to client API

---
+++ source/Octopus.Client/Model/Endpoints/KubernetesEndpointResource.cs
@@ -0,0 +1,17 @@
+﻿using Octopus.Client.Extensibility.Attributes;
+
+namespace Octopus.Client.Model.Endpoints
+{
+    class KubernetesEndpointResource : AgentlessEndpointResource
+    {
+        public override CommunicationStyle CommunicationStyle => CommunicationStyle.Kubernetes;
+
+        [Trim]
+        [Writeable]
+        public string AccountId { get; set; }
+
+        [Trim]
+        [Writeable]
+        public string ClusterUrl { get; set; }
+    }
+}
