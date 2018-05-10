0b7c3ecb1277d9908d16d62c8e455a0b696b33bd
Matthew Casperson <matthewcasperson@gmail.com>
2018-05-11 08:51:09 +1000

    Made class public

--- source/Octopus.Client/Model/Endpoints/KubernetesEndpointResource.cs
+++ source/Octopus.Client/Model/Endpoints/KubernetesEndpointResource.cs
@@ -2,7 +2,7 @@
 
 namespace Octopus.Client.Model.Endpoints
 {
-    class KubernetesEndpointResource : AgentlessEndpointResource
+    public class KubernetesEndpointResource : AgentlessEndpointResource
     {
         public override CommunicationStyle CommunicationStyle => CommunicationStyle.Kubernetes;
 
