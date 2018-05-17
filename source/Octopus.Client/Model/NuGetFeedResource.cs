287af9cd8c2507abe2f194d5157715ea419e6edb
Shannon Lewis <slewis74@gmail.com>
2018-05-17 16:58:23 +1000

    fixed NuGetFeedResource defaults, to retain backward compat

--- source/Octopus.Client/Model/NuGetFeedResource.cs
+++ source/Octopus.Client/Model/NuGetFeedResource.cs
@@ -4,15 +4,19 @@ namespace Octopus.Client.Model
 {
     public class NuGetFeedResource : FeedResource
     {
+        public static readonly int DefaultDownloadAttempts = 5;
+        public static readonly int DefaultDownloadRetryBackoffSeconds = 10;
+        public static readonly bool DefaultEnhancedMode = false;
+
         public override FeedType FeedType => FeedType.NuGet;
-        
-        [Writeable]
-        public int DownloadAttempts { get; set; }
 
         [Writeable]
-        public int DownloadRetryBackoffSeconds { get; set; }
+        public int DownloadAttempts { get; set; } = DefaultDownloadAttempts;
 
         [Writeable]
-        public bool EnhancedMode { get; set; }
+        public int DownloadRetryBackoffSeconds { get; set; } = DefaultDownloadRetryBackoffSeconds;
+
+        [Writeable]
+        public bool EnhancedMode { get; set; } = DefaultEnhancedMode;
     }
 }
