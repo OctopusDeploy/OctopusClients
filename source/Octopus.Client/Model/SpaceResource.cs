78e262d4feda580e663b7fe825b986cfb3dc6bd9
Jayden Platell <jayden.platell@octopus.com>
2018-05-28 11:01:02 +1000

    Added Space resource.

---
+++ source/Octopus.Client/Model/SpaceResource.cs
@@ -0,0 +1,24 @@
+﻿using Octopus.Client.Extensibility.Attributes;
+
+namespace Octopus.Client.Model
+{
+    public class SpaceResource
+    {
+        public SpaceResource()
+        {
+            Owners = new ReferenceCollection();
+        }
+
+        [Writeable]
+        public string Name { get; set; }
+
+        [Writeable]
+        public string Description { get; set; }
+
+        [Writeable]
+        public bool IsDefault { get; set; }
+
+        [Writeable]
+        public ReferenceCollection Owners { get; set; }
+    }
+}
