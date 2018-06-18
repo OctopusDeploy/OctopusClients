b7fbc1d681546dbcd51c69cde52eefc4e1e99c77
johnsimons <john.simons@octopus.com>
2018-06-18 11:52:08 +1000

    Removing feature flags

--- source/Octopus.Client/Model/NuGetFeedResource.cs
+++ source/Octopus.Client/Model/NuGetFeedResource.cs
@@ -2,17 +2,6 @@
 
 namespace Octopus.Client.Model
 {
-    public class BuiltInFeedResource : FeedResource
-    {
-        public override FeedType FeedType => FeedType.BuiltIn;
-
-        [Writeable]
-        public int? DeleteUnreleasedPackagesAfterDays { get; set; }
-
-        [Writeable]
-        public bool IsBuiltInRepoSyncEnabled { get; set; } = true;
-    }
-
     public class NuGetFeedResource : FeedResource
     {
         public static readonly int DefaultDownloadAttempts = 5;
