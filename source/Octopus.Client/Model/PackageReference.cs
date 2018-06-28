d3d06b75e5e924fa2a6a58121e954035adc43900
Michael Richardson <michael.james.richardson@gmail.com>
2018-06-29 09:51:33 +1000

    Added Packages collection to DeploymentActionResource

---
+++ source/Octopus.Client/Model/PackageReference.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Octopus.Client.Model
+{
+    public class PackageReference
+    {
+        string name;
+        
+        /// <summary>
+        /// Constructs a primary package (an unnamed package reference)
+        /// </summary>
+        public PackageReference(string packageId, string feedId, string acquisitionLocation)
+        :this(null, packageId, feedId, acquisitionLocation)
+        {
+        }
+        
+        /// <summary>
+        /// Constructs a primary package (an unnamed package reference)
+        /// </summary>
+        public PackageReference(string packageId, string feedId)
+        :this(packageId, feedId, PackageAcquisitionLocations.Server)
+        { }
+
+        [JsonConstructor]
+        public PackageReference(string name, string packageId, string feedId, string acquisitionLocation)
+            :this()
+        {
+            PackageId = packageId;
+            FeedId = feedId;
+            AcquisitionLocation = acquisitionLocation;
+            this.name = name;
+        }
+
+        public PackageReference()
+        {
+           Properties = new Dictionary<string, string>(); 
+        }
+        
+        public string Name
+        {
+            get => name ?? "";
+            set => name = value;
+        }
+        
+        public string PackageId { get; set; }
+        
+        public string FeedId { get; set; }
+        
+        public string AcquisitionLocation { get; set; }
+        
+        [JsonProperty(ObjectCreationHandling = ObjectCreationHandling.Reuse)]
+        public IDictionary<string, string> Properties { get; }
+        
+        [JsonIgnore]
+        public bool IsPrimaryPackage => Name == "";
+    }
+}
