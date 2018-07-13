dea8e225abb3ddbafc03ad40081fdcb4cd58abce
Rob <robert.erez@gmail.com>
2018-07-13 12:20:35 +1000

    Add Token Account & AWS ECR Feed

--- source/Octopus.Client/Model/FeedResource.cs
+++ source/Octopus.Client/Model/FeedResource.cs
@@ -4,27 +4,13 @@ using Octopus.Client.Extensibility.Attributes;
 
 namespace Octopus.Client.Model
 {
+    /// <summary>
+    /// The FeedResource type has been deprecated on Octopus Deploy 3.5 servers. Use NuGetFeedResource instead")]
+    /// </summary>
     public class FeedResource : Resource, INamedResource
     {
-        /// <summary>
-        /// The FeedResource type has been deprecated on Octopus Deploy 3.5 servers. Use NuGetFeedResource instead")]
-        /// </summary>
-        public FeedResource()
-        {
-            Password = new SensitiveValue();
-        }
-
         [Writeable]
         public string Name { get; set; }
-
-        [Writeable]
-        public string FeedUri { get; set; }
-
-        [Writeable]
-        public string Username { get; set; }
-
-        [Writeable]
-        public SensitiveValue Password { get; set; }
 
         [WriteableOnCreate]
         public virtual FeedType FeedType { get; }
