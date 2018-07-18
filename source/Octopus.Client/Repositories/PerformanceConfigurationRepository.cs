b86aab2fafc536a668fba4e2722b01046194fbef
Shane Gill <tothegills@gmail.com>
2018-07-18 14:51:51 +1000

    Add PerformanceConfiguration Relates to OctopusDeploy/Issues#4698

---
+++ source/Octopus.Client/Repositories/PerformanceConfigurationRepository.cs
@@ -0,0 +1,30 @@
+﻿using Octopus.Client.Model;
+
+namespace Octopus.Client.Repositories
+{
+    public interface IPerformanceConfigurationRepository
+    {
+        PerformanceConfigurationResource Get();
+        PerformanceConfigurationResource Modify(PerformanceConfigurationResource resource);
+    }
+
+    public class PerformanceConfigurationRepository : IPerformanceConfigurationRepository
+    {
+        readonly IOctopusClient client;
+
+        public PerformanceConfigurationRepository(IOctopusClient client)
+        {
+            this.client = client;
+        }
+
+        public PerformanceConfigurationResource Get()
+        {
+            return client.Get<PerformanceConfigurationResource>(client.RootDocument.Link("PerformanceConfiguration"));
+        }
+
+        public PerformanceConfigurationResource Modify(PerformanceConfigurationResource resource)
+        {
+            return client.Update(client.RootDocument.Link("PerformanceConfiguration"), resource);
+        }
+    }
+}
