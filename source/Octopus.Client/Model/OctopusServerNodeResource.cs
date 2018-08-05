07c4a9748b1f711ed5c85ee26714f96810ee67a6
Shane Gill <tothegills@gmail.com>
2018-08-06 08:47:42 +1000

    Revert "Add heartbeat policy to nodes"
    This reverts commit 4d61eb4505ea1ceb8410d88ec7996a6152517a01.

--- source/Octopus.Client/Model/OctopusServerNodeResource.cs
+++ source/Octopus.Client/Model/OctopusServerNodeResource.cs
@@ -4,20 +4,6 @@ using Octopus.Client.Extensibility.Attributes;
 
 namespace Octopus.Client.Model
 {
-    public class OctopusServerNodeHeartbeatPolicy
-    {
-        public static TimeSpan DefaultOfflineTimeout = TimeSpan.FromMinutes(30);
-
-        public enum HeartbeatDeletionPolicy
-        {
-            NeverDelete,
-            DeleteAfterOfflineTimeout
-        }
-
-        public HeartbeatDeletionPolicy DeletionPolicy { get; set; } = HeartbeatDeletionPolicy.NeverDelete;
-        public TimeSpan OfflineTimeout { get; set; } = DefaultOfflineTimeout;
-    }
-
     public class OctopusServerNodeResource : Resource, INamedResource
     {
         public string Name { get; set; }
@@ -28,7 +14,5 @@ namespace Octopus.Client.Model
         public int MaxConcurrentTasks { get; set; }
         [Writeable]
         public bool IsInMaintenanceMode { get; set; }
-        [Writeable]
-        public OctopusServerNodeHeartbeatPolicy HeartbeatPolicy { get; set; }
     }
 }
