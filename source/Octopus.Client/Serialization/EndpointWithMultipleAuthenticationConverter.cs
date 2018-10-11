a377a748dd561a4bd2bdfad493f8a42512ba6d31
Matthew Casperson <matthewcasperson@gmail.com>
2018-10-11 14:17:19 +1000

    Added converter

---
+++ source/Octopus.Client/Serialization/EndpointWithMultipleAuthenticationConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Octopus.Client.Model;
+using Octopus.Client.Model.Endpoints;
+
+namespace Octopus.Client.Serialization
+{
+    public class EndpointWithMultipleAuthenticationConverter : InheritedClassConverter<IEndpointWithMultipleAuthenticationResource, MultipleAccountType>
+    {
+        static readonly IDictionary<MultipleAccountType, Type> FeedTypeMappings =
+            new Dictionary<MultipleAccountType, Type>
+            {
+                {MultipleAccountType.KubernetesAzure, typeof(KubernetesAzureAuthenticationResource)},
+                {MultipleAccountType.KubernetesAws, typeof(KubernetesAwsAuthenticationResource)},
+                {MultipleAccountType.KubernetesCertificate, typeof(KubernetesCertificateAuthenticationResource)},
+                {MultipleAccountType.KubernetesStandard, typeof(KubernetesStandardAccountAuthenticationResource)},
+            };
+
+        protected override IDictionary<MultipleAccountType, Type> DerivedTypeMappings => FeedTypeMappings;
+        protected override string TypeDesignatingPropertyName => nameof(IEndpointWithMultipleAuthenticationResource.AuthenticationType);
+    }
+}
