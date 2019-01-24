d7a8c60549b5a2e7699608a23988e0d0bf59d16f
Shannon Lewis <slewis74@gmail.com>
2019-01-24 13:08:12 +1000

    update to the DeploymentPreviewResource to include the ReleaseNotes and WorkItems

--- source/Octopus.Client/Model/DeploymentPreviewResource.cs
+++ source/Octopus.Client/Model/DeploymentPreviewResource.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Octopus.Client.Model.Forms;
+using Octopus.Client.Model.IssueTrackers;
 
 namespace Octopus.Client.Model
 {
@@ -9,5 +10,8 @@ namespace Octopus.Client.Model
         public List<DeploymentTemplateStep> StepsToExecute { get; set; }
         public Form Form { get; set; }
         public bool UseGuidedFailureModeByDefault { get; set; }
+
+        public string ReleaseNotes { get; set; }
+        public List<WorkItem> WorkItems { get; set; }
     }
 }
