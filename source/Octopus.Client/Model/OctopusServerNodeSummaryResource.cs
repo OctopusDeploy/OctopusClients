c01d1840a2f55d4589ea81c7918ede34b05990bc
Matt Richardson <matt.richardson@octopus.com>
2019-01-30 09:20:58 +1100

    Add support for the OctopusServerNodes table split

---
+++ source/Octopus.Client/Model/OctopusServerNodeSummaryResource.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Octopus.Client.Extensibility;
+
+namespace Octopus.Client.Model
+{
+    public class OctopusServerNodeSummaryResource : Resource
+    {
+        public string Name { get; set; }
+        public int MaxConcurrentTasks { get; set; }
+        public bool IsInMaintenanceMode { get; set; }
+        public DateTimeOffset? LastSeen { get; set; }
+        public string Rank { get; set; }
+        public bool IsLeader { get; set; }
+        public bool IsOffline { get; set; }
+        public int RunningTaskCount { get;set; }
+    }
+}
