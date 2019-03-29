c137b87ddbf38430ec7e5672bd455d6ed3b213c0
NickJosevski <nick@josevski.com>
2019-03-29 11:52:58 +1100

    lets be precise what's missing

--- source/Octopus.Client/Model/UserPermissionSetResource.cs
+++ source/Octopus.Client/Model/UserPermissionSetResource.cs
@@ -31,8 +31,13 @@ namespace Octopus.Client.Model
         public List<ProjectedTeamReferenceDataItem> Teams { get; set; }
 
         /// <summary>
+        /// If the requesting user had sufficient access to see a complete view of the teams that may drive permissions
+        /// </summary>
+        public bool IsTeamsComplete { get; set; }
+
+        /// <summary>
         /// If the requesting user had sufficient access to see a complete view of the permissions
         /// </summary>
-        public bool IsComplete { get; set; }
+        public bool IsPermissionsComplete { get; set; }
     }
 }
