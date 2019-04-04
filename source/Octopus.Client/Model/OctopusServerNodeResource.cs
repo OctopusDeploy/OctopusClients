c106042c14b6230ac89d3e87e4d2aade17034572
Shannon Lewis <slewis74@gmail.com>
2019-04-04 16:59:15 +1000

    removed some properties that we incorrectly kept for current release

--- source/Octopus.Client/Model/OctopusServerNodeResource.cs
+++ source/Octopus.Client/Model/OctopusServerNodeResource.cs
@@ -12,18 +12,5 @@ namespace Octopus.Client.Model
         public int MaxConcurrentTasks { get; set; }
         [Writeable]
         public bool IsInMaintenanceMode { get; set; }
-        [WriteableOnCreate]
-        public int ServicesPort { get; set; }
-        [WriteableOnCreate]
-        public string WebSocketPrefix { get; set; }
-        [WriteableOnCreate]
-        public List<string> WebPortalListenPrefixes { get; set; }
-        [WriteableOnCreate]
-        public bool WebPortalForceSsl { get; set; }
-        [WriteableOnCreate]
-        public bool RequestLoggingEnabled { get; set; }
-        [WriteableOnCreate]
-        public bool RequestMetricLoggingEnabled { get; set; }
-
     }
 }
