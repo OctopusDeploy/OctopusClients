4854f64674aa0daeca8e542ed3fe10ed7f3fb0fc
Shannon Lewis <slewis74@gmail.com>
2019-05-01 08:17:41 +1000

    The ReleaseChanges now include CommitDetails

---
+++ source/Octopus.Client/Model/CommitDetails.cs
@@ -0,0 +1,11 @@
+namespace Octopus.Client.Model
+{
+    public class CommitDetails
+    {
+        public string Id { get; set; }
+
+        public string LinkUrl { get; set; }
+
+        public string Comment { get; set; }
+    }
+}
