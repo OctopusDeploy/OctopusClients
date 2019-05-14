8fa93d5c8dfdde5d8dba32977fdab97a1aaa5b5f
Jim Burger <jim.burger@octopus.com>
2019-05-14 11:36:31 +1000

    Updated assent

--- source/Octopus.Client/Repositories/DefaultSpaceNotFoundException.cs
+++ source/Octopus.Client/Repositories/DefaultSpaceNotFoundException.cs
@@ -3,7 +3,7 @@ using Octopus.Client.Extensibility;
 
 namespace Octopus.Client.Repositories
 {
-    internal class DefaultSpaceNotFoundException : Exception
+    public class DefaultSpaceNotFoundException : Exception
     {
         public DefaultSpaceNotFoundException(IHaveSpaceResource spaceResource) : base($"Couldn't find a default space while trying to create or modify {((IResource)spaceResource).Id}, this could either mean that the default space has been disabled, or you do not have access to the default space.")
         {
