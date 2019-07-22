c04c22e9f2b2440eea64927387a0474151772de4
Robert Wagner <robert@wagner.id.au>
2019-07-23 09:08:22 +1000

    Added cron options to MachineHealthCheckPolicy
    Related to https://github.com/OctopusDeploy/Issues/issues/5713

--- source/Octopus.Client/Model/MachineHealthCheckPolicy.cs
+++ source/Octopus.Client/Model/MachineHealthCheckPolicy.cs
@@ -5,13 +5,17 @@ namespace Octopus.Client.Model
 {
     public class MachineHealthCheckPolicy
     {
+        private const string UtcTimezone = "UTC";
+            
         [Obsolete("Use " + nameof(PowerShellHealthCheckPolicy) + " instead.")]
         public MachineScriptPolicy TentacleEndpointHealthCheckPolicy => PowerShellHealthCheckPolicy;
         public MachineScriptPolicy PowerShellHealthCheckPolicy { get; set; }
         [Obsolete("Use " + nameof(BashHealthCheckPolicy) + " instead.")]
         public MachineScriptPolicy SshEndpointHealthCheckPolicy => BashHealthCheckPolicy;
         public MachineScriptPolicy BashHealthCheckPolicy { get; set; }
-        public TimeSpan HealthCheckInterval { get; set; }
+        public TimeSpan? HealthCheckInterval { get; set; }
+        public string HealthCheckCron { get; set; }
+        public string HealthCheckCronTimezone { get; set; }
 
         public HealthCheckType HealthCheckType { get; set; }
 
@@ -21,6 +25,7 @@ namespace Octopus.Client.Model
             BashHealthCheckPolicy = new MachineScriptPolicy();
             HealthCheckInterval = TimeSpan.FromHours(1);
             HealthCheckType = HealthCheckType.RunScript;
+            HealthCheckCronTimezone = UtcTimezone;
         }
 
         [JsonConstructor]
