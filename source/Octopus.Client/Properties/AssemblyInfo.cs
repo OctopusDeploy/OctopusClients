9bb32a908f480659578e7cb67a5a04b5ce722ba6
Matt Richardson <matt.richardson@octopus.com>
2019-07-29 11:42:25 +1000

    Remove internalsvisibleto for octopuscli projects

--- source/Octopus.Client/Properties/AssemblyInfo.cs
+++ source/Octopus.Client/Properties/AssemblyInfo.cs
@@ -3,6 +3,4 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("Octopus.Client")]
-[assembly: InternalsVisibleTo("Octopus.Cli")]
 [assembly: InternalsVisibleTo("Octopus.Client.Tests")]
-[assembly: InternalsVisibleTo("Octo.Tests")]
