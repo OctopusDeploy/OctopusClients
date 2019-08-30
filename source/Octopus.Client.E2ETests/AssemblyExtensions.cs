b5c314f7e79b2cd7db1ee776806a3c2fd4784850
Matt Richardson <matt.richardson@octopus.com>
2019-08-30 15:03:41 +1000

    Add tests to ensure the packed, ilmerged artifacts work
    make sure the packed, ilmerged dll we're going to ship actually works the way we expect it to - ie, dependencies are correct etc

---
+++ source/Octopus.Client.E2ETests/AssemblyExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Octopus.Client.Model;
+
+namespace Octopus.Client.E2ETests
+{
+    internal static class AssemblyExtensions
+    {
+        public static string FullLocalPath(this Assembly assembly)
+        {
+            var codeBase = assembly.CodeBase;
+            var uri = new UriBuilder(codeBase);
+            var root = Uri.UnescapeDataString(uri.Path);
+            root = root.Replace('/',Path.DirectorySeparatorChar);
+            return root;
+        }
+    }
+}
---
+++ source/Octopus.Client.E2ETests/InlinedDependenciesFixture.cs
@@ -0,0 +1,82 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Reflection;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Octopus.Client.E2ETests
+{
+    [TestFixture]
+    public class InlinedDependenciesFixture
+    {
+        private Assembly assembly;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            var runtime = TestHelpers.GetRuntime();
+            var package = TestHelpers.GetNuGetPackage();
+
+            using (var zipFile = ZipFile.OpenRead(package))
+            {
+                var zipArchiveEntry =
+                    zipFile.Entries.FirstOrDefault(x => x.FullName.Contains("lib/" + runtime + "/Octopus.Client.dll"));
+                using (var stream = zipArchiveEntry.Open())
+                {
+                    using (var ms = new MemoryStream())
+                    {
+                        stream.CopyTo(ms);
+
+                        var rawAssembly = ms.ToArray();
+                        assembly = Assembly.Load(rawAssembly);
+                    }
+                }
+            }
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+
+        }
+#if SUPPORTS_ILMERGE
+        [Test]
+        public void HasInlinedNewtonsoftJson()
+        {
+            assembly.GetTypes()
+                .FirstOrDefault(x => x.FullName == "Newtonsoft.Json.JsonConvert")
+                .Should()
+                .NotBeNull(
+                    "We should have ilmerged Newtonsoft.Json in, so customers dont need to reference that separately");
+        }
+
+        [Test]
+        public void HasInlinedOctodiff()
+        {
+            assembly.GetTypes()
+                .FirstOrDefault(x => x.FullName == "Octodiff.Core.DeltaBuilder")
+                .Should()
+                .NotBeNull("We should have ilmerged Octodiff in, so customers dont need to reference that separately");
+        }
+#else
+        [Test]
+        public void HasNotInlinedNewtonsoftJson()
+        {
+            assembly.GetTypes()
+                .FirstOrDefault(x => x.FullName == "Newtonsoft.Json.JsonConvert")
+                .Should()
+                .BeNull("We aren't able to ilmerge Newtonsoft.Json in yet as it's only available from netcore 3");
+        }
+
+        [Test]
+        public void HasNotInlinedOctodiff()
+        {
+            assembly.GetTypes()
+                .FirstOrDefault(x => x.FullName == "Octodiff.Core.DeltaBuilder")
+                .Should()
+                .BeNull("We aren't able to ilmerge Octodiff in yet as it's only available from netcore 3");
+        }
+#endif
+    }
+}
