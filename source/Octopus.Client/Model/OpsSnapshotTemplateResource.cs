547f576fb3a3101a3d391c7e72873db1eed7bc5c
Mark Siedle <msiedle@gmail.com>
2019-09-19 11:04:11 +1000

    Sharing another base class for release templates

--- source/Octopus.Client/Model/OpsSnapshotTemplateResource.cs
+++ source/Octopus.Client/Model/OpsSnapshotTemplateResource.cs
@@ -2,13 +2,8 @@
 
 namespace Octopus.Client.Model
 {
-    public class OpsSnapshotTemplateResource : Resource
+    public class OpsSnapshotTemplateResource : ReleaseTemplateBaseResource
     {
         public string OpsStepsId { get; set; }
-        public string LastReleaseVersion { get; set; }
-        public string NextVersionIncrement { get; set; }
-        public string VersioningPackageStepName { get; set; }
-        public string VersioningPackageReferenceName { get; set; }
-        public IList<ReleaseTemplatePackage> Packages { get; set; }
     }
 }
