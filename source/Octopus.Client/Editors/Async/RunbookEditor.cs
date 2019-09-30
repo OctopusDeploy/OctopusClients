35a817557e2d7bfe64c20fe0b67a65f510b8a372
Mark Siedle <msiedle@gmail.com>
2019-09-30 16:26:46 +1000

    Updating runbook editor to include additional helpers to create easily for a project and return the equivalent steps collection (async version)

--- source/Octopus.Client/Editors/Async/RunbookEditor.cs
+++ source/Octopus.Client/Editors/Async/RunbookEditor.cs
@@ -8,13 +8,39 @@ namespace Octopus.Client.Editors.Async
     public class RunbookEditor : IResourceEditor<RunbookResource, RunbookEditor>
     {
         private readonly IRunbookRepository repository;
+        private readonly Lazy<Task<RunbookStepsEditor>> runbookSteps;
 
-        public RunbookEditor(IRunbookRepository repository)
+        public RunbookEditor(IRunbookRepository repository,
+            IRunbookStepsRepository runbookStepsRepository)
         {
             this.repository = repository;
+            runbookSteps = new Lazy<Task<RunbookStepsEditor>>(() => new RunbookStepsEditor(runbookStepsRepository).Load(Instance.RunbookStepsId));
         }
 
         public RunbookResource Instance { get; private set; }
+
+        public Task<RunbookStepsEditor> RunbookSteps => runbookSteps.Value;
+
+        public async Task<RunbookEditor> CreateOrModify(string projectId, string name)
+        {
+            var existing = await repository.FindByName(name);
+
+            if (existing == null)
+            {
+                Instance = await repository.Create(new RunbookResource
+                {
+                    Name = name,
+                    ProjectId = projectId,
+                });
+            }
+            else
+            {
+                existing.Name = name;
+                Instance = await repository.Modify(existing);
+            }
+
+            return this;
+        }
 
         public async Task<RunbookEditor> Load(string id)
         {
