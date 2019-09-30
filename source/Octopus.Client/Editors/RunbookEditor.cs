f1aac28e51bc317fd5c6512f6aabff2030210600
Mark Siedle <msiedle@gmail.com>
2019-09-30 16:08:00 +1000

    Updating runbook editor to include additional helpers to create easily for a project and return the equivalent steps colleciton

--- source/Octopus.Client/Editors/RunbookEditor.cs
+++ source/Octopus.Client/Editors/RunbookEditor.cs
@@ -7,13 +7,39 @@ namespace Octopus.Client.Editors
     public class RunbookEditor : IResourceEditor<RunbookResource, RunbookEditor>
     {
         private readonly IRunbookRepository repository;
+        private readonly Lazy<RunbookStepsEditor> runbookSteps;
 
-        public RunbookEditor(IRunbookRepository repository)
+        public RunbookEditor(IRunbookRepository repository,
+            IRunbookStepsRepository runbookStepsRepository)
         {
             this.repository = repository;
+            runbookSteps = new Lazy<RunbookStepsEditor>(() => new RunbookStepsEditor(runbookStepsRepository).Load(Instance.RunbookStepsId));
         }
 
         public RunbookResource Instance { get; private set; }
+
+        public RunbookStepsEditor RunbookSteps => runbookSteps.Value;
+
+        public RunbookEditor CreateOrModify(string projectId, string name)
+        {
+            var existing = repository.FindByName(name);
+
+            if (existing == null)
+            {
+                Instance = repository.Create(new RunbookResource
+                {
+                    Name = name,
+                    ProjectId = projectId,
+                });
+            }
+            else
+            {
+                existing.Name = name;
+                Instance = repository.Modify(existing);
+            }
+
+            return this;
+        }
 
         public RunbookEditor Load(string id)
         {
@@ -30,6 +56,10 @@ namespace Octopus.Client.Editors
         public RunbookEditor Save()
         {
             Instance = repository.Modify(Instance);
+            if (runbookSteps.IsValueCreated)
+            {
+                runbookSteps.Value.Save();
+            }
             return this;
         }
     }
