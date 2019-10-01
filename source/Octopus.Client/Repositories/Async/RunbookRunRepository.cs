d454f976d213ec277827c806d675ec7f899fd7d7
Mark Siedle <msiedle@gmail.com>
2019-10-01 17:57:48 +1000

    Adding runbooks lookup to find repositories for runbook runs

--- source/Octopus.Client/Repositories/Async/RunbookRunRepository.cs
+++ source/Octopus.Client/Repositories/Async/RunbookRunRepository.cs
@@ -12,13 +12,14 @@ namespace Octopus.Client.Repositories.Async
         /// 
         /// </summary>
         /// <param name="projects"></param>
+        /// <param name="runbooks"></param>
         /// <param name="environments"></param>
         /// <param name="skip">Number of records to skip</param>
         /// <param name="take">Number of records to take (First supported in Server 3.14.15)</param>
         /// <returns></returns>
-        Task<ResourceCollection<RunbookRunResource>> FindBy(string[] projects, string[] environments, int skip = 0, int? take = null);
-        Task Paginate(string[] projects, string[] environments, Func<ResourceCollection<RunbookRunResource>, bool> getNextPage);
-        Task Paginate(string[] projects, string[] environments, string[] tenants, Func<ResourceCollection<RunbookRunResource>, bool> getNextPage);
+        Task<ResourceCollection<RunbookRunResource>> FindBy(string[] projects, string[] runbooks, string[] environments, int skip = 0, int? take = null);
+        Task Paginate(string[] projects, string[] runbooks, string[] environments, Func<ResourceCollection<RunbookRunResource>, bool> getNextPage);
+        Task Paginate(string[] projects, string[] runbooks, string[] environments, string[] tenants, Func<ResourceCollection<RunbookRunResource>, bool> getNextPage);
     }
 
     class RunbookRunRepository : BasicRepository<RunbookRunResource>, IRunbookRunRepository
@@ -33,19 +34,19 @@ namespace Octopus.Client.Repositories.Async
             return Client.Get<TaskResource>(resource.Link("Task"));
         }
 
-        public async Task<ResourceCollection<RunbookRunResource>> FindBy(string[] projects, string[] environments, int skip = 0, int? take = null)
+        public async Task<ResourceCollection<RunbookRunResource>> FindBy(string[] projects, string[] runbooks, string[] environments, int skip = 0, int? take = null)
         {
-            return await Client.List<RunbookRunResource>(await Repository.Link("RunbookRuns").ConfigureAwait(false), new { skip, take, projects = projects ?? new string[0], environments = environments ?? new string[0] }).ConfigureAwait(false);
+            return await Client.List<RunbookRunResource>(await Repository.Link("RunbookRuns").ConfigureAwait(false), new { skip, take, projects = projects ?? new string[0], runbooks = runbooks ?? new string[0], environments = environments ?? new string[0] }).ConfigureAwait(false);
         }
 
-        public Task Paginate(string[] projects, string[] environments, Func<ResourceCollection<RunbookRunResource>, bool> getNextPage)
+        public Task Paginate(string[] projects, string[] runbooks, string[] environments, Func<ResourceCollection<RunbookRunResource>, bool> getNextPage)
         {
-            return Paginate(projects, environments, new string[0], getNextPage);
+            return Paginate(projects, runbooks, environments, new string[0], getNextPage);
         }
 
-        public async Task Paginate(string[] projects, string[] environments, string[] tenants, Func<ResourceCollection<RunbookRunResource>, bool> getNextPage)
+        public async Task Paginate(string[] projects, string[] runbooks, string[] environments, string[] tenants, Func<ResourceCollection<RunbookRunResource>, bool> getNextPage)
         {
-            await Client.Paginate(await Repository.Link("RunbookRuns").ConfigureAwait(false), new { projects = projects ?? new string[0], environments = environments ?? new string[0], tenants = tenants ?? new string[0] }, getNextPage).ConfigureAwait(false);
+            await Client.Paginate(await Repository.Link("RunbookRuns").ConfigureAwait(false), new { projects = projects ?? new string[0], runbooks = runbooks ?? new string[0], environments = environments ?? new string[0], tenants = tenants ?? new string[0] }, getNextPage).ConfigureAwait(false);
         }
     }
 }
--- source/Octopus.Client/Repositories/RunbookRunRepository.cs
+++ source/Octopus.Client/Repositories/RunbookRunRepository.cs
@@ -11,13 +11,14 @@ namespace Octopus.Client.Repositories
         /// 
         /// </summary>
         /// <param name="projects"></param>
+        /// <param name="runbooks"></param>
         /// <param name="environments"></param>
         /// <param name="skip">Number of records to skip</param>
         /// <param name="take">Number of records to take (First supported in Server 3.14.159)</param>
         /// <returns></returns>
-        ResourceCollection<RunbookRunResource> FindBy(string[] projects, string[] environments, int skip = 0, int? take = null);
-        void Paginate(string[] projects, string[] environments, Func<ResourceCollection<RunbookRunResource>, bool> getNextPage);
-        void Paginate(string[] projects, string[] environments, string[] tenants, Func<ResourceCollection<RunbookRunResource>, bool> getNextPage);
+        ResourceCollection<RunbookRunResource> FindBy(string[] projects, string[] runbooks, string[] environments, int skip = 0, int? take = null);
+        void Paginate(string[] projects, string[] runbooks, string[] environments, Func<ResourceCollection<RunbookRunResource>, bool> getNextPage);
+        void Paginate(string[] projects, string[] runbooks, string[] environments, string[] tenants, Func<ResourceCollection<RunbookRunResource>, bool> getNextPage);
 
     }
 
@@ -33,19 +34,19 @@ namespace Octopus.Client.Repositories
             return Client.Get<TaskResource>(resource.Link("Task"));
         }
 
-        public ResourceCollection<RunbookRunResource> FindBy(string[] projects, string[] environments, int skip = 0, int? take = null)
+        public ResourceCollection<RunbookRunResource> FindBy(string[] projects, string[] runbooks, string[] environments, int skip = 0, int? take = null)
         {
-            return Client.List<RunbookRunResource>(Repository.Link("RunbookRuns"), new { skip, take, projects = projects ?? new string[0], environments = environments ?? new string[0] });
+            return Client.List<RunbookRunResource>(Repository.Link("RunbookRuns"), new { skip, take, projects = projects ?? new string[0], runbooks = runbooks ?? new string[0], environments = environments ?? new string[0] });
         }
 
-        public void Paginate(string[] projects, string[] environments, Func<ResourceCollection<RunbookRunResource>, bool> getNextPage)
+        public void Paginate(string[] projects, string[] runbooks, string[] environments, Func<ResourceCollection<RunbookRunResource>, bool> getNextPage)
         {
-            Paginate(projects, environments, new string[0], getNextPage);
+            Paginate(projects, runbooks, environments, new string[0], getNextPage);
         }
 
-        public void Paginate(string[] projects, string[] environments, string[] tenants, Func<ResourceCollection<RunbookRunResource>, bool> getNextPage)
+        public void Paginate(string[] projects, string[] runbooks, string[] environments, string[] tenants, Func<ResourceCollection<RunbookRunResource>, bool> getNextPage)
         {
-            Client.Paginate(Repository.Link("RunbookRuns"), new { projects = projects ?? new string[0], environments = environments ?? new string[0], tenants = tenants ?? new string[0] }, getNextPage);
+            Client.Paginate(Repository.Link("RunbookRuns"), new { projects = projects ?? new string[0], runbooks = runbooks ?? new string[0], environments = environments ?? new string[0], tenants = tenants ?? new string[0] }, getNextPage);
         }
     }
 }
