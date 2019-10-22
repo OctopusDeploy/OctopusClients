8c6eb5f4fd9548088b56d7ed60b17796156582e4
Ankith Konda <ankith.konda@octopus.com>
2019-10-22 14:08:40 +1000

    remove unnecessary fields

--- source/Octopus.Client/Model/ProjectReference.cs
+++ source/Octopus.Client/Model/ProjectReference.cs
@@ -5,29 +5,14 @@ namespace Octopus.Client.Model
 {
     public class ProjectReference
     {
-        public ProjectReference(string projectId, string projectName, string projectSlug, string projectGroupId, string projectGroupName, List<DateTime> timestamps)
+        public ProjectReference(string projectId, List<DateTime> timestamps)
         {
             ProjectId = projectId;
-            ProjectName = projectName;
-            ProjectSlug = projectSlug;
-            ProjectGroupId = projectGroupId;
-            ProjectGroupName = projectGroupName;
             TimeStamps = timestamps;
             Score = 100;
         }
-
         public string ProjectId { get; }
-
-        public string ProjectName { get; }
-
-        public string ProjectSlug { get; }
-
-        public string ProjectGroupId { get; }
-
-        public string ProjectGroupName { get; }
-
         public List<DateTime> TimeStamps { get; }
-
         public int Score { get; set; }
     }
 }
