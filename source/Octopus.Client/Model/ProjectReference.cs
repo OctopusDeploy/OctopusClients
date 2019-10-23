2a1c5e6f5df1edab3ea080d360f2d5077e31fcf5
Ankith Konda <ankith.konda@octopus.com>
2019-10-23 14:53:19 +1000

    add AccessCount to projectReference

--- source/Octopus.Client/Model/ProjectReference.cs
+++ source/Octopus.Client/Model/ProjectReference.cs
@@ -8,5 +8,6 @@ namespace Octopus.Client.Model
         public string ProjectId { get; }
         public IReadOnlyList<DateTime> TimeStamps { get; }
         public int Score { get; }
+        public int AccessCount { get; }
     }
 }
