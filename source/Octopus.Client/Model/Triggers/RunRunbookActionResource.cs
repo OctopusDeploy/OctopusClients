94d07b64d973de8f00d7425ebcb4d6ebf0d1a8b6
Ali Almaktoum <ali.almaktoum@octopus.com>
2019-11-07 16:00:42 +1000

    Added TriggerActionResource

---
+++ source/Octopus.Client/Model/Triggers/RunRunbookActionResource.cs
@@ -0,0 +1,16 @@
+﻿using Octopus.Client.Extensibility.Attributes;
+
+namespace Octopus.Client.Model.Triggers
+{
+    public class RunRunbookActionResource : ScopedDeploymentActionResource
+    {
+        public override TriggerActionType ActionType => TriggerActionType.RunRunbook;
+
+        [Writeable] 
+        public string RunbookId { get; set; }
+        
+        [Writeable]
+        public ReferenceCollection EnvironmentIds { get; set; }
+        
+    }
+}
