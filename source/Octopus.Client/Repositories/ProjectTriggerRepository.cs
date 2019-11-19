c0a73d1354a07c7c46386c16023daf2f3d5c1483
Nick Josevski <nick@josevski.com>
2019-11-19 15:18:11 +1100

    Project triggers structure change in 2019.11

--- source/Octopus.Client/Repositories/ProjectTriggerRepository.cs
+++ source/Octopus.Client/Repositories/ProjectTriggerRepository.cs
@@ -16,15 +16,20 @@ namespace Octopus.Client.Repositories
         public ProjectTriggerRepository(IOctopusRepository repository)
             : base(repository, "ProjectTriggers")
         {
+            MinimumCompatibleVersion("2019.11.0");
         }
 
         public ProjectTriggerResource FindByName(ProjectResource project, string name)
         {
+            ThrowIfServerVersionIsNotCompatible();
+            
             return FindByName(name, path: project.Link("Triggers"));
         }
 
         public ProjectTriggerEditor CreateOrModify(ProjectResource project, string name, TriggerFilterResource filter, TriggerActionResource action)
         {
+            ThrowIfServerVersionIsNotCompatible();
+            
             return new ProjectTriggerEditor(this).CreateOrModify(project, name, filter, action);
         }
     }
