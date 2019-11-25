bfdfcb332a339ccd33e205ff6a0199a83042fcff
Mark Siedle <msiedle@gmail.com>
2019-11-25 12:54:56 +1100

    Adding support for Runbook Publishing (#480)
    * Adding run endpoint
    
    * Updating resources for run endpoint
    
    * Removing published run resource and using the RunbookRunResource instead, and adding necessary link collection lookup
    
    * Adding required message to RunbookSnapshotId property
    
    * Removing TODO

--- source/Octopus.Client/Repositories/Async/RunbookRepository.cs
+++ source/Octopus.Client/Repositories/Async/RunbookRepository.cs
@@ -8,6 +8,10 @@ namespace Octopus.Client.Repositories.Async
     {
         Task<RunbookResource> FindByName(ProjectResource project, string name);
         Task<RunbookEditor> CreateOrModify(ProjectResource project, string name, string description);
+        Task<RunbookSnapshotTemplateResource> GetRunbookSnapshotTemplate(RunbookResource runbook);
+        Task<RunbookRunTemplateResource> GetRunbookRunTemplate(RunbookResource runbook);
+        Task<RunbookRunPreviewResource> GetPreview(DeploymentPromotionTarget promotionTarget);
+        Task<RunbookRunResource> Run(RunbookResource runbook, RunbookRunResource runbookRun);
     }
 
     class RunbookRepository : BasicRepository<RunbookResource>, IRunbookRepository
@@ -25,6 +29,26 @@ namespace Octopus.Client.Repositories.Async
         public Task<RunbookEditor> CreateOrModify(ProjectResource project, string name, string description)
         {
             return new RunbookEditor(this, new RunbookProcessRepository(Repository)).CreateOrModify(project, name, description);
+        }
+
+        public Task<RunbookSnapshotTemplateResource> GetRunbookSnapshotTemplate(RunbookResource runbook)
+        {
+            return Client.Get<RunbookSnapshotTemplateResource>(runbook.Link("RunbookSnapshotTemplate"));
+        }
+
+        public Task<RunbookRunTemplateResource> GetRunbookRunTemplate(RunbookResource runbook)
+        {
+            return Client.Get<RunbookRunTemplateResource>(runbook.Link("RunbookRunTemplate"));
+        }
+
+        public Task<RunbookRunPreviewResource> GetPreview(DeploymentPromotionTarget promotionTarget)
+        {
+            return Client.Get<RunbookRunPreviewResource>(promotionTarget.Link("RunbookRunPreview"));
+        }
+
+        public Task<RunbookRunResource> Run(RunbookResource runbook, RunbookRunResource runbookRun)
+        {
+            return Client.Post<object, RunbookRunResource>(runbook.Link("CreateRunbookRun"), runbookRun);
         }
     }
 }
--- source/Octopus.Client/Repositories/RunbookRepository.cs
+++ source/Octopus.Client/Repositories/RunbookRepository.cs
@@ -7,6 +7,10 @@ namespace Octopus.Client.Repositories
     {
         RunbookResource FindByName(ProjectResource project, string name);
         RunbookEditor CreateOrModify(ProjectResource project, string name, string description);
+        RunbookSnapshotTemplateResource GetRunbookSnapshotTemplate(RunbookResource runbook);
+        RunbookRunTemplateResource GetRunbookRunTemplate(RunbookResource runbook);
+        RunbookRunPreviewResource GetPreview(DeploymentPromotionTarget promotionTarget);
+        RunbookRunResource Run(RunbookResource runbook, RunbookRunResource runbookRun);
     }
     
     class RunbookRepository : BasicRepository<RunbookResource>, IRunbookRepository
@@ -24,6 +28,26 @@ namespace Octopus.Client.Repositories
         public RunbookEditor CreateOrModify(ProjectResource project, string name, string description)
         {
             return new RunbookEditor(this, new RunbookProcessRepository(Repository)).CreateOrModify(project, name, description);
+        }
+
+        public RunbookSnapshotTemplateResource GetRunbookSnapshotTemplate(RunbookResource runbook)
+        {
+            return Client.Get<RunbookSnapshotTemplateResource>(runbook.Link("RunbookSnapshotTemplate"));
+        }
+
+        public RunbookRunTemplateResource GetRunbookRunTemplate(RunbookResource runbook)
+        {
+            return Client.Get<RunbookRunTemplateResource>(runbook.Link("RunbookRunTemplate"));
+        }
+
+        public RunbookRunPreviewResource GetPreview(DeploymentPromotionTarget promotionTarget)
+        {
+            return Client.Get<RunbookRunPreviewResource>(promotionTarget.Link("RunbookRunPreview"));
+        }
+
+        public RunbookRunResource Run(RunbookResource runbook, RunbookRunResource runbookRun)
+        {
+            return Client.Post<object, RunbookRunResource>(runbook.Link("CreateRunbookRun"), runbookRun);
         }
     }
 }
