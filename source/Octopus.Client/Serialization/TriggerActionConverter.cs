b27246cc53d890cc4cc4f3fa842c50520eb313ef
Ali Almaktoum <ali.almaktoum@gmail.com>
2019-11-26 01:16:01 +1100

    Client update for run Runbook on a schedule (#479)
    * Added TriggerActionResource
    
    * Added TriggerActionConverter for RunBookActionResource
    
    * add tenants and update to triggeractionresource
    
    * Add filtering by runbook ids

--- source/Octopus.Client/Serialization/TriggerActionConverter.cs
+++ source/Octopus.Client/Serialization/TriggerActionConverter.cs
@@ -8,12 +8,13 @@ namespace Octopus.Client.Serialization
     public class TriggerActionConverter : InheritedClassConverter<TriggerActionResource, TriggerActionType>
     {
         static readonly IDictionary<TriggerActionType, Type> ActionTypes =
-          new Dictionary<TriggerActionType, Type>
-          {
-              { TriggerActionType.AutoDeploy, typeof (AutoDeployActionResource)},
-              { TriggerActionType.DeployLatestRelease, typeof (DeployLatestReleaseActionResource)},
-              { TriggerActionType.DeployNewRelease, typeof (DeployNewReleaseActionResource)}
-          };
+            new Dictionary<TriggerActionType, Type>
+            {
+                {TriggerActionType.AutoDeploy, typeof(AutoDeployActionResource)},
+                {TriggerActionType.DeployLatestRelease, typeof(DeployLatestReleaseActionResource)},
+                {TriggerActionType.DeployNewRelease, typeof(DeployNewReleaseActionResource)},
+                {TriggerActionType.RunRunbook, typeof(RunRunbookActionResource)}
+            };
 
         protected override IDictionary<TriggerActionType, Type> DerivedTypeMappings => ActionTypes;
         protected override string TypeDesignatingPropertyName => "ActionType";
