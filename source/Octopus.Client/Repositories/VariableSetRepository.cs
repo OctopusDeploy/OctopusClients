b4cf2f3e58cc9d9ee67b918cdff21d376565ff76
Mark Siedle <msiedle@gmail.com>
2020-02-17 16:20:07 +1000

    Adding missing endpoints to variables repositories

--- source/Octopus.Client/Repositories/VariableSetRepository.cs
+++ source/Octopus.Client/Repositories/VariableSetRepository.cs
@@ -1,14 +1,13 @@
 using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
-using Octopus.Client.Exceptions;
 using Octopus.Client.Model;
 
 namespace Octopus.Client.Repositories
 {
-    public interface IVariableSetRepository : IGet<VariableSetResource>, IModify<VariableSetResource>
+    public interface IVariableSetRepository : IGet<VariableSetResource>, IModify<VariableSetResource>, IGetAll<VariableSetResource>
     {
         string[] GetVariableNames(string projects, string[] environments);
+        VariableSetResource GetVariablePreview(string project, string channel, string tenant, string runbook, string action, string environment, string machine, string role);
     }
     
     class VariableSetRepository : BasicRepository<VariableSetResource>, IVariableSetRepository
@@ -21,6 +20,11 @@ namespace Octopus.Client.Repositories
         public string[] GetVariableNames(string project, string[] environments)
         {
             return Client.Get<string[]>(Repository.Link("VariableNames"), new { project, projectEnvironmentsFilter = environments ?? new string[0] });
+        }
+
+        public VariableSetResource GetVariablePreview(string project, string channel, string tenant, string runbook, string action, string environment, string machine, string role)
+        {
+            return Client.Get<VariableSetResource>(Repository.Link("VariablePreview"), new { project, channel, tenant, runbook, action, environment, machine, role });
         }
 
         public override List<VariableSetResource> Get(params string[] ids)
