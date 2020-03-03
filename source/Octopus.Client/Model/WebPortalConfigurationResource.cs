44e956c5f214fe181572a2084ec86c2abfd63c04
Ali Almaktoum <ali.almaktoum@octopus.com>
2020-03-03 16:54:45 +1000

    Added XFrameOptions

--- source/Octopus.Client/Model/WebPortalConfigurationResource.cs
+++ source/Octopus.Client/Model/WebPortalConfigurationResource.cs
@@ -45,9 +45,14 @@ namespace Octopus.Client.Model
 
     public class XOptionsResource
     {
-        public const string XFrameAllowFromDescription = "A uri to provide in the X-Frame-Option http header in conjunction with the ALLOW-FROM value.";
+        public const string XFrameAllowFromDescription = "(Deprecated) A uri to provide in the X-Frame-Option http header in conjunction with the ALLOW-FROM value.";
+        public const string XFrameOptionsDescription = "Provide in the X-Frame-Option http header a directive such as sameorigin or deny.";
+
 
         [Writeable]
         public string XFrameOptionAllowFrom { get; set; }
+        
+        [Writeable]
+        public string XFrameOptions { get; set; }
     }
 }
