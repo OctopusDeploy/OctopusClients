6fd98112ea4f4c095f2c3b68f8fafffdebfa72f0
Shannon Lewis <slewis74@gmail.com>
2020-03-12 15:15:02 +1000

    cleaned up using statement

--- source/Octopus.Client/OctopusRepository.cs
+++ source/Octopus.Client/OctopusRepository.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Net;
 using System.Threading;
 using Octopus.Client.Exceptions;
-using Octopus.Client.Logging;
 using Octopus.Client.Model;
 using Octopus.Client.Repositories;
 
