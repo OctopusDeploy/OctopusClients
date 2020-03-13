a2b17548399a3bc999589d03483fe3f7edd88b5e
Shannon Lewis <slewis74@gmail.com>
2020-03-13 10:27:49 +1000

    forgot that registry node objects are disposable

--- source/Octopus.Client/LocationChecker.cs
+++ source/Octopus.Client/LocationChecker.cs
@@ -14,26 +14,31 @@ namespace Octopus.Client
 
         public static void CheckAssemblyLocation()
         {
-            var octopusRegNode = Registry.LocalMachine.OpenSubKey(@"Software\Octopus");
-            CheckBasedOnApplication(octopusRegNode, "Server");
-            CheckBasedOnApplication(octopusRegNode, "Tentacle");
+            using (var octopusRegNode = Registry.LocalMachine.OpenSubKey(@"Software\Octopus"))
+            {
+                CheckBasedOnApplication(octopusRegNode, "Server");
+                CheckBasedOnApplication(octopusRegNode, "Tentacle");
+            }
         }
 
         private static void CheckBasedOnApplication(RegistryKey octopusRegNode, string application)
         {
-            var applicationRegNode = octopusRegNode.OpenSubKey(application);
-            if (applicationRegNode != null)
+            using (var applicationRegNode = octopusRegNode.OpenSubKey(application))
             {
-                var installationFolder = (string) applicationRegNode.GetValue("InstallLocation");
-                var currentAssemblyLocation = typeof(LocationChecker).Assembly.Location;
-
-                if (currentAssemblyLocation.Contains(installationFolder))
+                if (applicationRegNode != null)
                 {
-                    var warningMessage = $"Using Octopus.Client from the {application}'s installation folder has been deprecated. In future versions it may not be shipped with {application}. Please see https://g.octopushq.com/usingoctopusclientfrominstallationfolder.";
-                    
-                    // Write this to the log, if one is configured. Also write it to the console in case no log has been configured
-                    Logger.Warn(warningMessage);
-                    Console.WriteLine(warningMessage);
+                    var installationFolder = (string) applicationRegNode.GetValue("InstallLocation");
+                    var currentAssemblyLocation = typeof(LocationChecker).Assembly.Location;
+
+                    if (currentAssemblyLocation.Contains(installationFolder))
+                    {
+                        var warningMessage =
+                            $"Using Octopus.Client from the {application}'s installation folder has been deprecated. In future versions it may not be shipped with {application}. Please see https://g.octopushq.com/usingoctopusclientfrominstallationfolder.";
+
+                        // Write this to the log, if one is configured. Also write it to the console in case no log has been configured
+                        Logger.Warn(warningMessage);
+                        Console.WriteLine(warningMessage);
+                    }
                 }
             }
         }
