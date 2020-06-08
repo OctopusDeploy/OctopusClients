c3f352d6abea63935e2c430cd38b1cecb44460c9
Matthew Casperson <matthewcasperson@gmail.com>
2020-06-08 15:03:30 +1000

    Added fields to client library

--- source/Octopus.Client/Model/Endpoints/EndpointResource.cs
+++ source/Octopus.Client/Model/Endpoints/EndpointResource.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using Octopus.Client.Extensibility.Attributes;
 
 namespace Octopus.Client.Model.Endpoints
 {
     public abstract class EndpointResource : Resource
     {
         public abstract CommunicationStyle CommunicationStyle { get; }
+        
+        [Writeable]
+        public bool RunningInContainer { get; set; }
+
+        [Writeable]
+        public DeploymentActionContainer Container { get; set; }
     }
 }
