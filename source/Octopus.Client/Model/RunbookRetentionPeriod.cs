3624158aa4e722051174c89ede54cfda9f10c4ee
Andrew Best <andrew.best@octopus.com>
2020-06-17 11:56:53 +0930

    Adds RetentionPeriod to Runbook resource

---
+++ source/Octopus.Client/Model/RunbookRetentionPeriod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Octopus.Client.Model
+{
+    public class RunbookRetentionPeriod
+    {
+        public RunbookRetentionPeriod(int quantityToKeep)
+        {
+            if (quantityToKeep < 0)
+                throw new ArgumentOutOfRangeException(nameof(quantityToKeep), "Quantity to keep must be 0 (indicating keep forever) or greater");
+
+            QuantityToKeep = quantityToKeep;
+        }
+
+        public int QuantityToKeep { get; }
+
+        public bool ShouldKeepForever => QuantityToKeep == 0;
+
+        public static RetentionPeriod KeepForever()
+        {
+            return new RetentionPeriod(0, RetentionUnit.Items);
+        }
+    }
+}
