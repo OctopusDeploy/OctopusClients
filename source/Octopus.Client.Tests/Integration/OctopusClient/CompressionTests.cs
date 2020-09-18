ecdbc11a4f27f9d478167e1cfc3a60f63c3a369c
Adam McCoy <adam.mccoy@octopus.com>
2020-09-18 15:10:35 +1000

    Add test fixture for compression headers

---
+++ source/Octopus.Client.Tests/Integration/OctopusClient/CompressionTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Nancy;
+using NUnit.Framework;
+
+namespace Octopus.Client.Tests.Integration.OctopusClient
+{
+    public class CompressionTests : HttpIntegrationTestBase
+    {
+        private static readonly string[] ExpectedValues = { "deflate", "gzip" };
+        
+        public CompressionTests()
+            : base(UrlPathPrefixBehaviour.UseClassNameAsUrlPathPrefix)
+        {
+            Get(TestRootPath, p =>
+            {
+                var acceptEncoding = Request.Headers.AcceptEncoding;
+
+                return Response.AsJson(new TestDto { AcceptEncoding = acceptEncoding })
+                    .WithStatusCode(HttpStatusCode.OK);
+            });
+        }
+
+        [Test]
+        public async Task AsyncClient_ShouldProvideAcceptEncoding()
+        {
+            var response = await AsyncClient.Get<TestDto>(TestRootPath);
+            response.AcceptEncoding.Should().Contain(ExpectedValues);
+        }
+
+        [Test]
+        public void SyncClient_ShouldProvideAcceptEncoding()
+        {
+            var client = new Client.OctopusClient(new OctopusServerEndpoint(HostBaseUri + TestRootPath));
+            var response = client.Get<TestDto>(TestRootPath);
+            response.AcceptEncoding.Should().Contain(ExpectedValues);
+        }
+
+        private class TestDto
+        {
+            public IEnumerable<string> AcceptEncoding { get; set; }
+        }
+    }
+}
