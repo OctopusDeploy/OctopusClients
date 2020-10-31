014a2ae060e9d6c9801977964fc9dc68f89585cf
Adam McCoy <adam.mccoy@octopus.com>
2020-10-31 15:21:01 +1100

    Use separate models for listing and creating API keys

--- source/Octopus.Client/Model/ApiKeyResource.cs
+++ source/Octopus.Client/Model/ApiKeyResource.cs
@@ -3,15 +3,21 @@ using Octopus.Client.Extensibility.Attributes;
 
 namespace Octopus.Client.Model
 {
-    public class ApiKeyResource : Resource
+    public abstract class ApiKeyResourceBase : Resource
     {
         [WriteableOnCreate]
         public string Purpose { get; set; }
-
         public string UserId { get; set; }
-        public string ApiKey { get; set; }
-        public string ApiKeyMasked { get; set; }
-        public string ApiKeyHint { get; set; }
         public DateTimeOffset Created { get; set; }
+    }
+
+    public class ApiKeyResource : ApiKeyResourceBase
+    {
+        public SensitiveValue ApiKey { get; set; }
+    }
+
+    public class ApiKeyCreatedResource : ApiKeyResourceBase
+    {
+        public string ApiKey { get; set; }
     }
 }
