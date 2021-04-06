54eb058d3fde9d9851b544deefac0c464cee7f50
Laurence Evans <laurence.evanz@gmail.com>
2021-04-06 17:56:34 +1000

    Expose new RetentionDefaultConfigurationResource

---
+++ source/Octopus.Client/Model/RetentionDefaultConfigurationResource.cs
@@ -0,0 +1,10 @@
+﻿using Octopus.Client.Extensibility.Attributes;
+
+namespace Octopus.Client.Model
+{
+    public class RetentionDefaultConfigurationResource : Resource
+    {
+        [Writeable]
+        public int? RetentionDays { get; set; }
+    }
+}
