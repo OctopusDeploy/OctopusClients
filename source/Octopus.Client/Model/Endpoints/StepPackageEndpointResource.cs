16c375e889cf5aec898395647055db63bc258beb
Egor Pavlikhin <egor.pavlikhin@gmail.com>
2021-05-20 13:45:36 +1000

    Add StepPackageEndpoint resource to enable Intergration tests in Server

---
+++ source/Octopus.Client/Model/Endpoints/StepPackageEndpointResource.cs
@@ -0,0 +1,13 @@
+﻿using Octopus.Client.Extensibility.Attributes;
+
+namespace Octopus.Client.Model.Endpoints
+{
+    public class StepPackageEndpointResource : EndpointResource
+    {
+        public override CommunicationStyle CommunicationStyle => CommunicationStyle.StepPackage;
+
+        [Trim]
+        [Writeable]
+        public string Inputs { get; set; }
+    }
+}
