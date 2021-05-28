91bfed26d337f3773cf6ba3de05d8b814776603b
Eoin Motherway <eoin.motherway@octopus.com>
2021-05-28 11:48:19 +1000

    Made URL helper internal

--- source/Octopus.Client/UrlHelper.cs
+++ source/Octopus.Client/UrlHelper.cs
@@ -2,12 +2,12 @@ using Octopus.Client.Extensibility;
 
 namespace Octopus.Client
 {
-    public static class UrlHelper
+    internal static class UrlHelper
     {
         /// <summary>
         ///     Returns the "Self" link from the provided resource if available, otherwise returns <c>null</c> 
         /// </summary>
-        public static string GetSelfUrlOrNull<TResource>(TResource resource)
+        internal static string GetSelfUrlOrNull<TResource>(TResource resource)
         {
             const string self = "Self";
             if (resource is IResource res && res.Links.ContainsKey(self))
