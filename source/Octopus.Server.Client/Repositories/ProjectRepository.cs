79ab1adcac5792dd890185eb720682d6143491b2
Robert <robert.erez@gmail.com>
2021-09-02 20:44:41 +1000

    Remove exceptions blocking use of channels on original endpoint

--- source/Octopus.Server.Client/Repositories/ProjectRepository.cs
+++ source/Octopus.Server.Client/Repositories/ProjectRepository.cs
@@ -60,10 +60,6 @@ namespace Octopus.Client.Repositories
 
         public ResourceCollection<ChannelResource> GetChannels(ProjectResource project)
         {
-            if (project.PersistenceSettings is VersionControlSettingsResource)
-                throw new NotSupportedException(
-                    $"Version Controlled projects are still in Beta. Use {nameof(IProjectBetaRepository)}.");
-            
             return Client.List<ChannelResource>(project.Link("Channels"));
         }
 
