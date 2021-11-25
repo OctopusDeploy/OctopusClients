dd11d7995ab5bf602d26f089ae2e43a356baf61b
Ankith Konda <ankith.konda@gmail.com>
2021-11-25 16:40:00 +1000

    reuse method

--- source/Octopus.Server.Client/Repositories/TelemetryConfigurationRepository.cs
+++ source/Octopus.Server.Client/Repositories/TelemetryConfigurationRepository.cs
@@ -33,7 +33,7 @@ namespace Octopus.Client.Repositories
 
         public TelemetryConfigurationResource EnableTelemetry()
         {
-            return repository.Client.Update(repository.Link(LinkName), new TelemetryConfigurationResource
+            return ModifyTelemetryConfiguration(new TelemetryConfigurationResource
             {
                 Enabled = true
             });
@@ -41,7 +41,7 @@ namespace Octopus.Client.Repositories
 
         public TelemetryConfigurationResource DisableTelemetry()
         {
-            return repository.Client.Update(repository.Link(LinkName), new TelemetryConfigurationResource
+            return ModifyTelemetryConfiguration(new TelemetryConfigurationResource
             {
                 Enabled = false
             });
