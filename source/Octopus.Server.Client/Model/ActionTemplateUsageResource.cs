dcb82e375fbaccd4f3d96fa3e66155fe3c0ca72a
LEE MEYER <lee.meyer@gmail.com>
2022-03-10 16:58:33 +1100

    Revert "Merge pull request #644 from OctopusDeploy/meyer/actiontempusages"
    This reverts commit 9dd7fda47f214015f7045af5775907b87d1e1ab0, reversing
    changes made to f89a04b5c4c020a7b0f75e4451f1effd26bf636b.

--- source/Octopus.Server.Client/Model/ActionTemplateUsageResource.cs
+++ source/Octopus.Server.Client/Model/ActionTemplateUsageResource.cs
@@ -20,7 +20,5 @@ namespace Octopus.Client.Model
 
         [Obsolete("Use " + nameof(ProcessId) + " with " + nameof(ProcessType) + " instead")]
         public string DeploymentProcessId { get; set; }
-        public string Branch { get; set; }
-        public string Release { get; set; }
     }
 }
