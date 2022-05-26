c26f8d1ec813cb6bcb98faa73c6083edbf6398f8
Susan Pan <82346985+susanpann@users.noreply.github.com>
2022-05-27 08:46:15 +1000

    Add SelectedPackagePublished field to ReleaseTemplatePackage model (#655)
    * add selectedpackagepublish date field
    
    * add to publicSurfaceFixture

--- source/Octopus.Server.Client/Model/ReleaseTemplatePackage.cs
+++ source/Octopus.Server.Client/Model/ReleaseTemplatePackage.cs
@@ -44,6 +44,7 @@ namespace Octopus.Client.Model
         public string FeedName { get; set; }
 
         public string VersionSelectedLastRelease { get; set; }
+        public DateTimeOffset? SelectedPackagePublished { get; set; }
 
         /// <summary>
         /// Gets or sets a value indicating whether the <see cref="PackageId" /> or <see cref="FeedId" /> contain no
