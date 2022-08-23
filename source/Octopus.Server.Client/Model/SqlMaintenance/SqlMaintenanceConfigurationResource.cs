44f12b11833b7ddc2e0d71851818d246b3f601bd
Stephen Burman <stephen.burman@octopus.com>
2022-08-23 17:16:02 +1000

    Adding SqlMaintenanceConfigurationResource to the Octopus client

---
+++ source/Octopus.Server.Client/Model/SqlMaintenance/SqlMaintenanceConfigurationResource.cs
@@ -0,0 +1,29 @@
+﻿using Octopus.Client.Extensibility;
+using Octopus.Client.Extensibility.Attributes;
+
+namespace Octopus.Client.Model.SqlMaintenance
+{
+    public class SqlMaintenanceConfigurationResource : IResource
+    {
+        public SqlMaintenanceConfigurationResource()
+        {
+            Id = "sqlmaintenance";
+        }
+
+        public string Id { get; set; }
+
+        [Writeable]
+        public bool IsIndexMaintenanceEnabled { get; set; }
+
+        [Writeable]
+        public string IndexMaintenanceSchedule { get; set; } = "0 1 * * SUN";
+
+        [Writeable]
+        public bool IsStatisticsUpdateEnabled { get; set; }
+
+        [Writeable]
+        public string StatisticsUpdateSchedule { get; set; } = "0 4 * * SUN";
+
+        public LinkCollection Links { get; set; }
+    }
+}
