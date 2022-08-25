960bb205d3c19966f7a44cda7e229e013c68a4a6
susanpann <susan.pan@octopus.com>
2022-08-25 10:04:53 +1000

    add upgradeavailable to tentacledetailsResource

--- source/Octopus.Server.Client/Model/Endpoints/TentacleDetailsResource.cs
+++ source/Octopus.Server.Client/Model/Endpoints/TentacleDetailsResource.cs
@@ -10,5 +10,6 @@ namespace Octopus.Client.Model.Endpoints
         public string Version { get; set; }
         public bool UpgradeSuggested { get; set; }
         public bool UpgradeRequired { get; set; }
+        public bool UpgradeAvailable { get; set; }
     }
 }
