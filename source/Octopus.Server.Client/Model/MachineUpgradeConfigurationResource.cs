e6d1fd7ebdcf81640a333e01b4e42fc865b2afdd
Nelson Susanto <nelson.susanto@octopus.com>
2022-09-08 10:08:25 +1000

    Move machine upgrades to its own resource

---
+++ source/Octopus.Server.Client/Model/MachineUpgradeConfigurationResource.cs
@@ -0,0 +1,18 @@
+﻿using Octopus.Client.Extensibility;
+using Octopus.Client.Extensibility.Attributes;
+
+namespace Octopus.Client.Model;
+
+public class MachineUpgradeConfigurationResource : IResource
+{
+    public MachineUpgradeConfigurationResource()
+    {
+        Id = "machineupgrades";
+    }
+
+    [Writeable]
+    public int? MachineTaskProcessingLimit { get; set; }
+
+    public string Id { get; }
+    public LinkCollection Links { get; set; }
+}
