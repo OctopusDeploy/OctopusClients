6bf8b66fc6d17e7139b52511f9a1e55d5fbc0a61
michaelongso <michael.ongso@octopus.com>
2022-09-12 09:17:24 +1000

    Marked method as obsolete directing to overload with cancellation token

--- source/Octopus.Server.Client/Repositories/Async/ICreate.cs
+++ source/Octopus.Server.Client/Repositories/Async/ICreate.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -5,6 +6,7 @@ namespace Octopus.Client.Repositories.Async
 {
     public interface ICreate<TResource>
     {
+        [Obsolete("Please use the overload with cancellation token instead.", false)]
         Task<TResource> Create(TResource resource, object pathParameters = null);
 
         Task<TResource> Create(TResource resource, CancellationToken cancellationToken, object pathParameters = null);
--- source/Octopus.Server.Client/Repositories/Async/IModify.cs
+++ source/Octopus.Server.Client/Repositories/Async/IModify.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -5,6 +6,7 @@ namespace Octopus.Client.Repositories.Async
 {
     public interface IModify<TResource>
     {
+        [Obsolete("Please use the overload with cancellation token instead.", false)]
         Task<TResource> Modify(TResource resource);
 
         Task<TResource> Modify(TResource resource, CancellationToken cancellationToken);
