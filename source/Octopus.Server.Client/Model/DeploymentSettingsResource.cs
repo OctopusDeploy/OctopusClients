50770459827b1e74445715c2fcdc404bf7e6df3e
Chris Kim <chris.kim@octopus.com>
2023-01-11 10:05:22 +1300

    Add ForcePackageDownload to Runbook, Project & ProjectSetting resources

--- source/Octopus.Server.Client/Model/DeploymentSettingsResource.cs
+++ source/Octopus.Server.Client/Model/DeploymentSettingsResource.cs
@@ -28,5 +28,8 @@ namespace Octopus.Client.Model
 
         [Writeable]
         public string DeploymentChangesTemplate { get; set; }
+
+        [Writeable]
+        public bool ForcePackageDownload { get; set; }
     }
 }
