2d8f52fc239ab7922be3c72fb8b6e26f3645aa1c
Nelson Susanto <nelson.susanto@octopus.com>
2023-01-27 16:07:39 +1100

    Add TrustedProxies to Web Portal config (#725)

---
+++ source/Octopus.Server.Client/Model/WebPortalConfiguration/WebPortalSecurityResource.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Octopus.Client.Extensibility.Attributes;
+
+namespace Octopus.Client.Model.WebPortalConfiguration;
+
+public class WebPortalSecurityResource
+{
+    [Writeable]
+    public string CorsWhitelist { get; set; }
+
+    [Writeable]
+    public string ReferrerPolicy { get; set; }
+
+    [Writeable]
+    public bool ContentSecurityPolicyEnabled { get; set; }
+
+    [Writeable]
+    public bool HttpStrictTransportSecurityEnabled { get; set; }
+
+    [Writeable]
+    [Required]
+    public long HttpStrictTransportSecurityMaxAge { get; set; }
+
+    public XOptionsResource XOptions { get; set; } = new XOptionsResource();
+}
