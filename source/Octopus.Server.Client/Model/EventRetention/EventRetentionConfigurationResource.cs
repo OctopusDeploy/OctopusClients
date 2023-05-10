858de0c41de45c9b72c0d4831959285d6b0c5963
isaac <isaac.calligeros@octopus.com>
2023-05-10 12:47:28 +0930

    Add Cron Expression to Event Retention resource

--- source/Octopus.Server.Client/Model/EventRetention/EventRetentionConfigurationResource.cs
+++ source/Octopus.Server.Client/Model/EventRetention/EventRetentionConfigurationResource.cs
@@ -12,9 +12,12 @@ namespace Octopus.Client.Model.EventRetention
         }
 
         public string Id { get; set; }
-        
+
         [Writeable]
         public int EventRetentionDays { get; set; } = 90;
+
+        [Writeable]
+        public string CronExpression { get; set; } = "0 */4 * * *";
 
         public LinkCollection Links { get; set; }
     }
