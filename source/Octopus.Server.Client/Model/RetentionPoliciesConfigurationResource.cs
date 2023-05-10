a9aa75e82289283315dff7dafdd03e8b1caf2e48
isaac <isaac.calligeros@octopus.com>
2023-05-10 14:26:04 +0930

    Add retention policy configuration

---
+++ source/Octopus.Server.Client/Model/RetentionPoliciesConfigurationResource.cs
@@ -0,0 +1,19 @@
+using Octopus.Client.Extensibility;
+using Octopus.Client.Extensibility.Attributes;
+
+namespace Octopus.Client.Model
+{
+    public class RetentionPoliciesConfigurationResource : IResource
+    {
+        public RetentionPoliciesConfigurationResource()
+        {
+            Id = "retentionpolicies";
+        }
+
+        public string Id { get; set; }
+
+        [Writeable] public string CronExpression { get; set; } = "0 */4 * * *";
+
+        public LinkCollection Links { get; set; }
+    }
+}
