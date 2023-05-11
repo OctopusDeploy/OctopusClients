c61a3feb856efb6bb96aa46005c9cd18cb5cf927
isaac <isaac.calligeros@octopus.com>
2023-05-11 13:23:02 +0930

    Update to use Resource over IResource

--- source/Octopus.Server.Client/Model/RetentionPoliciesConfigurationResource.cs
+++ source/Octopus.Server.Client/Model/RetentionPoliciesConfigurationResource.cs
@@ -3,18 +3,14 @@ using Octopus.Client.Extensibility.Attributes;
 
 namespace Octopus.Client.Model
 {
-    public class RetentionPoliciesConfigurationResource : IResource
+    public class RetentionPoliciesConfigurationResource : Resource
     {
         public RetentionPoliciesConfigurationResource()
         {
             Id = "retentionpolicies";
         }
 
-        public string Id { get; set; }
-
         [Writeable]
         public string CronExpression { get; set; } = "0 */4 * * *";
-
-        public LinkCollection Links { get; set; }
     }
 }
