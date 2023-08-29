a660f210b7cbaea2e41ddae6cee70982e8720821
veo.chen <veo.chen@octopus.com>
2023-08-29 15:26:49 +1200

    async editor

---
+++ source/Octopus.Server.Client/Editors/Async/AzureOidcAccountEditor.cs
@@ -0,0 +1,12 @@
+﻿using Octopus.Client.Model.Accounts;
+using Octopus.Client.Repositories.Async;
+
+namespace Octopus.Client.Editors.Async
+{
+    public class AzureOidcAccountEditor : AccountEditor<AzureSubscriptionAccountResource, AzureOidcAccountEditor>
+    {
+        public AzureOidcAccountEditor(IAccountRepository repository) : base(repository)
+        {
+        }
+    }
+}
