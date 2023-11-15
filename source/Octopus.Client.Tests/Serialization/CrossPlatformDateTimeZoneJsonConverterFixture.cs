aa6b533d8324a8fb6c61c89c410ab097088f52f4
Scott Merchant <scott.merchant@octopus.com>
2023-11-15 14:54:59 +1030

    Remove unnecessary comments

--- source/Octopus.Client.Tests/Serialization/CrossPlatformDateTimeZoneJsonConverterFixture.cs
+++ source/Octopus.Client.Tests/Serialization/CrossPlatformDateTimeZoneJsonConverterFixture.cs
@@ -39,7 +39,7 @@ namespace Octopus.Client.Tests.Serialization
 
             date
                 .Should()
-                .BeCloseTo(expected, TimeSpan.FromSeconds(1)); // Within one second
+                .BeCloseTo(expected, TimeSpan.FromSeconds(1));
         }
 
         [Test]
@@ -57,7 +57,7 @@ namespace Octopus.Client.Tests.Serialization
 
             date
                 .Should()
-                .BeCloseTo(expected, TimeSpan.FromSeconds(1)); // Within one second
+                .BeCloseTo(expected, TimeSpan.FromSeconds(1));
         }
 
         [Test]
