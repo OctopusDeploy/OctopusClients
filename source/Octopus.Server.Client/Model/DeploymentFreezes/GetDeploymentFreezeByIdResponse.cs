39cbcbb3da5e5afb84cb3d544688d9a906f9f4f8
Chris Kim <97423717+mik-ky@users.noreply.github.com>
2024-02-09 10:40:18 +1300

    Add DeploymentFreezes.Get (#821)
    * Add DeploymentFreezes.Get
    
    * Implement freeze get
    
    * Clean up client methods
    
    * Approvals
    
    ---------
    
    Co-authored-by: Shane <tothegills@gmail.com>

---
+++ source/Octopus.Server.Client/Model/DeploymentFreezes/GetDeploymentFreezeByIdResponse.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Octopus.Client.Model.DeploymentFreezes;
+
+public class GetDeploymentFreezeByIdResponse
+{
+    [Required]
+    public string Id { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    [Required]
+    public DateTimeOffset Start { get; set; }
+
+    [Required]
+    public DateTimeOffset End { get; set; }
+
+    [Required]
+    public Dictionary<string, ReferenceCollection> ProjectEnvironmentScope { get; protected set; }
+}
