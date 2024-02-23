6b71f6bb6427e9163b44985ea746bba1ed4066ae
Scott Merchant <scott.merchant@octopus.com>
2024-02-23 15:37:34 +1030

    Fix property names

--- source/Octopus.Server.Client/Model/Endpoints/KubernetesAgentDetailsResource.cs
+++ source/Octopus.Server.Client/Model/Endpoints/KubernetesAgentDetailsResource.cs
@@ -2,10 +2,10 @@ namespace Octopus.Client.Model.Endpoints
 {
     public class KubernetesAgentDetailsResource
     {
-        public string KubernetesAgentVersion { get; set; }
+        public string Version { get; set; }
 
         public string HelmReleaseName { get; set; }
 
-        public string KubernetesAgentNamespace { get; set; }
+        public string Namespace { get; set; }
     }
 }
