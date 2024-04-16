d6a33c9440d393da16377be40d70485977a62e3a
Scott Merchant <scott.merchant@octopus.com>
2024-04-16 11:57:52 +0930

    Add tentacle version property

--- source/Octopus.Server.Client/Model/Endpoints/KubernetesAgentDetailsResource.cs
+++ source/Octopus.Server.Client/Model/Endpoints/KubernetesAgentDetailsResource.cs
@@ -4,6 +4,8 @@ namespace Octopus.Client.Model.Endpoints
     {
         public string Version { get; set; }
 
+        public string TentacleVersion { get; set; }
+
         public string HelmReleaseName { get; set; }
 
         public string Namespace { get; set; }
