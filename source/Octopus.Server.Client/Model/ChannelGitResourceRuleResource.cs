227083353a6994219787af030ee5c9003e80f673
Geoff Lamrock <geoff.lamrock@gmail.com>
2024-06-27 10:17:42 +1000

    Add missing id property

--- source/Octopus.Server.Client/Model/ChannelGitResourceRuleResource.cs
+++ source/Octopus.Server.Client/Model/ChannelGitResourceRuleResource.cs
@@ -4,6 +4,7 @@ namespace Octopus.Client.Model
 {
     public class ChannelGitResourceRuleResource
     {
+        public string Id { get; set; }
         public List<DeploymentActionGitDependencyResource> GitDependencyActions { get; set; }
         public List<string> Rules { get; set; }
     }
