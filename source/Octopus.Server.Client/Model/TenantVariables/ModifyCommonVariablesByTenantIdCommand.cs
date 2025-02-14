f87be64bdcfe3bacc61313499cd9ea74164aa650
Bec Callow <bec.callow@octopus.com>
2025-02-14 10:30:05 +1000

    Split common variable types for command and response

--- source/Octopus.Server.Client/Model/TenantVariables/ModifyCommonVariablesByTenantIdCommand.cs
+++ source/Octopus.Server.Client/Model/TenantVariables/ModifyCommonVariablesByTenantIdCommand.cs
@@ -1,35 +1,26 @@
 ﻿namespace Octopus.Client.Model.TenantVariables;
 
-public class ModifyCommonVariablesByTenantIdCommand(string tenantId, string spaceId, TenantCommonVariable[] variables)
+public class ModifyCommonVariablesByTenantIdCommand(string tenantId, string spaceId, TenantCommonVariablePayload[] variables)
 {
     public string TenantId { get; set; } = tenantId;
 
     public string SpaceId { get; set; } = spaceId;
 
-    public TenantCommonVariable[] Variables { get; set; } = variables;
+    public TenantCommonVariablePayload[] Variables { get; set; } = variables;
 }
 
 
-public class TenantCommonVariable(string libraryVariableSetId, string templateId, PropertyValueResource value, CommonVariableScope scope)
+public class TenantCommonVariablePayload(string libraryVariableSetId, string templateId, PropertyValueResource value, TenantCommonVariable.CommonVariableScope scope)
 {
     public string Id { get; set; } = string.Empty;
 
     public string LibraryVariableSetId { get; set; } = libraryVariableSetId;
-    
-    public string LibraryVariableSetName { get; set; }
 
     public string TemplateId { get; set; } = templateId;
 
-    public ActionTemplateParameterResource Template { get; set; }
-
     public PropertyValueResource Value { get; set; } = value;
 
-    public CommonVariableScope Scope { get; set; } = scope;
-}
-
-public class CommonVariableScope(ReferenceCollection environmentIds)
-{
-    public ReferenceCollection EnvironmentIds { get; set; } = environmentIds;
+    public TenantCommonVariable.CommonVariableScope Scope { get; set; } = scope;
 }
 
 public class ModifyCommonVariablesByTenantIdResponse(string tenantId, TenantCommonVariable[] commonVariables)
---
+++ source/Octopus.Server.Client/Model/TenantVariables/TenantCommonVariable.cs
@@ -0,0 +1,26 @@
+﻿namespace Octopus.Client.Model.TenantVariables;
+
+public class TenantCommonVariable
+{
+    public class TenantCommonVariablePayload(string libraryVariableSetId, string templateId, PropertyValueResource value, CommonVariableScope scope)
+    {
+        public string Id { get; set; } = string.Empty;
+
+        public string LibraryVariableSetId { get; set; } = libraryVariableSetId;
+    
+        public string LibraryVariableSetName { get; set; }
+
+        public string TemplateId { get; set; } = templateId;
+
+        public ActionTemplateParameterResource Template { get; set; }
+
+        public PropertyValueResource Value { get; set; } = value;
+
+        public CommonVariableScope Scope { get; set; } = scope;
+    }
+    
+    public class CommonVariableScope(ReferenceCollection environmentIds)
+    {
+        public ReferenceCollection EnvironmentIds { get; set; } = environmentIds;
+    }
+}
