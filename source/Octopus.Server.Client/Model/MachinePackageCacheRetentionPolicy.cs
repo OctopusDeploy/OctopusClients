d2ef67a2a74333f93656824a9938f6dcedc5515d
Bec Callow <bec.callow@octopus.com>
2025-05-21 14:15:11 +1000

    Add cache retention to machine policy

---
+++ source/Octopus.Server.Client/Model/MachinePackageCacheRetentionPolicy.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Octopus.Client.Model;
+
+public class MachinePackageCacheRetentionPolicy
+{
+    public int QuantityToKeep { get; set; }
+    public MachinePackageCacheRetentionUnit Unit { get; set; }
+}
---
+++ source/Octopus.Server.Client/Model/MachinePackageCacheRetentionUnit.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Octopus.Client.Model
+{
+    public enum MachinePackageCacheRetentionUnit
+    {
+        Items
+    }
+}
