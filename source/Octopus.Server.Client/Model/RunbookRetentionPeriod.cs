40560bf05d1c7370cfb9bfa401740c2042c74c3c
William Laugesen <will.laugesen@octopus.com>
2025-06-12 14:28:43 +1200

    Updated RunbookRetentionPeriod to support RetentionUnit (#937)
    Introduces the Unit property as an enum to the RunbookRetentionPeriod model in Octopus.Client to match the server's contract.

--- source/Octopus.Server.Client/Model/RunbookRetentionPeriod.cs
+++ source/Octopus.Server.Client/Model/RunbookRetentionPeriod.cs
@@ -1,9 +1,10 @@
-﻿namespace Octopus.Client.Model
-{
-    public class RunbookRetentionPeriod
-    {
-        public int QuantityToKeep { get; set; }
+﻿namespace Octopus.Client.Model;
 
-        public bool ShouldKeepForever { get; set; }
-    }
+public class RunbookRetentionPeriod
+{
+    public int QuantityToKeep { get; set; }
+
+    public bool ShouldKeepForever { get; set; }
+
+    public RunbookRetentionUnit Unit { get; set; } = RunbookRetentionUnit.Items;
 }
