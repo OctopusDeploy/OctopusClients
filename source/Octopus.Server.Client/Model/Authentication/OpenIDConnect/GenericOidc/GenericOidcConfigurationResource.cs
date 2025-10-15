8743c61fca500d7fb1626b2ea781693668a85844
TerryAtOctopus <225791365+TerryAtOctopus@users.noreply.github.com>
2025-10-15 14:59:41 +1000

    New changes for the configuration of the generic OIDC auth provider
    skip-release-and-release-note: true
    GitOrigin-RevId: b5a9040c3b09aeeb2b34c74a58761ba6f57cd089

---
+++ source/Octopus.Server.Client/Model/Authentication/OpenIDConnect/GenericOidc/GenericOidcConfigurationResource.cs
@@ -0,0 +1,26 @@
+﻿#nullable enable
+using System.ComponentModel;
+using Octopus.Client.Extensibility.Attributes;
+
+namespace Octopus.Client.Model.Authentication.OpenIDConnect.GenericOidc
+{
+    // TODO: Correct 'learn more' link
+    [Description("Sign in to your Octopus Server with OIDC. [Learn more](https://g.octopushq.com/AuthOidc).")]
+    public class GenericOidcConfigurationResource : OpenIDConnectConfigurationResource
+    {
+        public GenericOidcConfigurationResource()
+        {
+            Id = "authentication-generic-oidc";
+        }
+
+        [DisplayName("Role Claim Type")]
+        [Description("Tell Octopus how to find the roles in the security token from the OIDC provider")]
+        [Writeable]
+        public string? RoleClaimType { get; set; }
+
+        [DisplayName("Username Claim Type")]
+        [Description("Tell Octopus how to find the value for the Octopus Username in the OIDC token. Defaults to \"preferred_username\" if left blank.")]
+        [Writeable]
+        public string? UsernameClaimType { get; set; }
+    }
+}
