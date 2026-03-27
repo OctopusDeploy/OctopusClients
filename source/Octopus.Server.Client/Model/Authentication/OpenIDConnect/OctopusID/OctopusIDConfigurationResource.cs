1056e60dc3fdf0b9407a14ad1d39d691f7aeb071
x-cubed <11729+x-cubed@users.noreply.github.com>
2026-03-27 14:18:50 +1300

    Adds AllowDynamicRegistration to the OctopusIDConfigurationResource
    +semver: minor
    
    GitOrigin-RevId: 8d7d49701bf3f897b616a55bc91f12a4bc93cc85

--- source/Octopus.Server.Client/Model/Authentication/OpenIDConnect/OctopusID/OctopusIDConfigurationResource.cs
+++ source/Octopus.Server.Client/Model/Authentication/OpenIDConnect/OctopusID/OctopusIDConfigurationResource.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using Octopus.Client.Extensibility.Attributes;
 
 namespace Octopus.Client.Model.Authentication.OpenIDConnect.OctopusID
 {
@@ -9,5 +10,10 @@ namespace Octopus.Client.Model.Authentication.OpenIDConnect.OctopusID
         {
             Id = "authentication-octopusid";
         }
+
+        [DisplayName("Allow Dynamic Registration")]
+        [Description("Allow Octopus Server to register itself automatically as a client for Octopus ID")]
+        [Writeable]
+        public bool? AllowDynamicRegistration { get; set; }
     }
 }
