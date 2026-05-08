33498221c3d48d59fbf497504d6f36ccd182ce90
nathanwoctopusdeploy <86938706+nathanwoctopusdeploy@users.noreply.github.com>
2026-05-08 11:07:38 +1000

    Add Octopus ID M2M configuration properties
    +semver: minor
    
    GitOrigin-RevId: e1f112453cd742ee63a3c6a56e013b8571d1a8cb

--- source/Octopus.Server.Client/Model/Authentication/OpenIDConnect/OctopusID/OctopusIDConfigurationResource.cs
+++ source/Octopus.Server.Client/Model/Authentication/OpenIDConnect/OctopusID/OctopusIDConfigurationResource.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel;
+#nullable enable
+using System;
+using System.ComponentModel;
 using Octopus.Client.Extensibility.Attributes;
 
 namespace Octopus.Client.Model.Authentication.OpenIDConnect.OctopusID
@@ -15,5 +17,25 @@ namespace Octopus.Client.Model.Authentication.OpenIDConnect.OctopusID
         [Description("Allow Octopus Server to register itself automatically as a client for Octopus ID")]
         [Writeable]
         public bool? AllowDynamicRegistration { get; set; }
+
+        [DisplayName("Allow M2M Dynamic Registration")]
+        [Description("Allow Octopus Server to register itself automatically as an M2M client for Octopus ID")]
+        [Writeable]
+        public bool? AllowM2MDynamicRegistration { get; set; }
+
+        [DisplayName("M2M Client ID")]
+        [Description("The client ID issued by Octopus ID via the M2M dynamic-registration flow.")]
+        [Writeable]
+        public string? M2MClientId { get; set; }
+
+        [DisplayName("M2M Resource Server Identifier")]
+        [Description("The resource server identifier issued by Octopus ID via the M2M dynamic-registration flow.")]
+        [Writeable]
+        public string? M2MResourceServerIdentifier { get; set; }
+
+        [DisplayName("M2M Registered At")]
+        [Description("The timestamp at which Octopus Server was registered as an M2M client with Octopus ID.")]
+        [Writeable]
+        public DateTimeOffset? M2MRegisteredAt { get; set; }
     }
 }
